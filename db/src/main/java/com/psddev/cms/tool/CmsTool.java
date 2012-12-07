fd7bf3046994d891da3a01ad52170691dfc3902e
Dan Slaughter <dan@perfectsensedigital.com>
2012-12-07 11:34:15 -0500

    Additional revert for commit bc638c0e2e7284ca6318c67b8744b48dddad0874 "Hack to fix permalinks not saving by reverting."

--- db/src/main/java/com/psddev/cms/tool/CmsTool.java
+++ db/src/main/java/com/psddev/cms/tool/CmsTool.java
@@ -7,6 +7,8 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+
 /** Brightspot CMS. */
 public class CmsTool extends Tool {
 
@@ -227,4 +229,80 @@ public class CmsTool extends Tool {
 
         return plugins;
     }*/
+
+    @Override
+    public void initialize(Logger logger) throws Exception {
+
+        // Pages & Content area.
+        introducePlugin(createArea("Pages & Content", "dashboard", null, "/"));
+
+        // Admin area.
+        Area admin = createArea("Admin", "admin", null, "/admin/");
+        introducePlugin(admin);
+
+        introducePlugin(createArea("Settings", "adminSettings", admin, "/admin/settings.jsp"));
+        introducePlugin(createArea("Sites", "adminSites", admin, "/admin/sites.jsp"));
+        introducePlugin(createArea("Templates & Sections", "adminTemplates", admin, "/admin/templates.jsp"));
+        introducePlugin(createArea("Types", "adminTypes", admin, "/admin/types.jsp"));
+        introducePlugin(createArea("URLs", "adminUrls", admin, "/admin/urls.jsp"));
+        introducePlugin(createArea("Users & Roles", "adminUsers", admin, "/admin/users.jsp"));
+        introducePlugin(createArea("Variations & Profiles", "adminVariations", admin, "/admin/variations.jsp"));
+        introducePlugin(createArea("Workflows", "adminWorkflows", admin, "/admin/workflows.jsp"));
+
+        logger.info("Initialized the areas.");
+
+        // Content right widgets.
+        double rightColumn = 0.0;
+        double rightRow = 0.0;
+
+        JspWidget drafts = createWidget(JspWidget.class, "Drafts", "drafts", "table_multiple");
+        drafts.setJsp("/WEB-INF/widget/drafts.jsp");
+        drafts.addPosition(CONTENT_RIGHT_WIDGET_POSITION, rightColumn, rightRow ++);
+
+        JspWidget schedules = createWidget(JspWidget.class, "Schedules", "schedules", "calendar");
+        schedules.setJsp("/WEB-INF/widget/schedules.jsp");
+        schedules.addPosition(CONTENT_RIGHT_WIDGET_POSITION, rightColumn, rightRow ++);
+
+        JspWidget urls = createWidget(JspWidget.class, "URLs", "urls", "folder_page");
+        urls.setJsp("/WEB-INF/widget/urls.jsp");
+        urls.addPosition(CONTENT_RIGHT_WIDGET_POSITION, rightColumn, rightRow ++);
+
+        JspWidget template = createWidget(JspWidget.class, "Template", "template", "layout");
+        template.setJsp("/WEB-INF/widget/template.jsp");
+        template.addPosition(CONTENT_RIGHT_WIDGET_POSITION, rightColumn, rightRow ++);
+
+        JspWidget sites = createWidget(JspWidget.class, "Sites", "sites", "application_cascade");
+        sites.setJsp("/WEB-INF/widget/sites.jsp");
+        sites.addPosition(CONTENT_RIGHT_WIDGET_POSITION, rightColumn, rightRow ++);
+
+        JspWidget history = createWidget(JspWidget.class, "History", "history", "table_multiple");
+        history.setJsp("/WEB-INF/widget/history.jsp");
+        history.addPosition(CONTENT_RIGHT_WIDGET_POSITION, rightColumn, rightRow ++);
+
+        JspWidget references = createWidget(JspWidget.class, "References", "references", "table_multiple");
+        references.setJsp("/WEB-INF/widget/references.jsp");
+        references.addPosition(CONTENT_RIGHT_WIDGET_POSITION, rightColumn, rightRow ++);
+
+        urls.getUpdateDependencies().add(template);
+
+        introducePlugin(drafts);
+        introducePlugin(schedules);
+        introducePlugin(urls);
+        introducePlugin(template);
+        introducePlugin(sites);
+        introducePlugin(history);
+        introducePlugin(references);
+
+        // Content bottom widgets.
+        double bottomColumn = 0.0;
+        double bottomRow = 0.0;
+
+        JspWidget seo = createWidget(JspWidget.class, "Search Engine Optimization", "seo", "page_white_magnify");
+        seo.setJsp("/WEB-INF/widget/seo.jsp");
+        seo.addPosition(CONTENT_BOTTOM_WIDGET_POSITION, bottomColumn, bottomRow ++);
+
+        introducePlugin(seo);
+
+        logger.info("Initialized the widgets.");
+    }
 }
