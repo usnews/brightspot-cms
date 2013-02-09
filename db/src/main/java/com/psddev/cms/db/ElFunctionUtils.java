f96c150432360939e29f55695d1fc251e1e26882
Hyoo Lim <hyoolim@gmail.com>
2013-02-09 13:17:46 -0500

    Adds cms:html and cms:js EL functions.

--- db/src/main/java/com/psddev/cms/db/ElFunctionUtils.java
+++ db/src/main/java/com/psddev/cms/db/ElFunctionUtils.java
@@ -3,10 +3,32 @@ package com.psddev.cms.db;
 import java.util.List;
 
 import com.psddev.dari.db.State;
+import com.psddev.dari.util.StringUtils;
 
 public final class ElFunctionUtils {
 
     private ElFunctionUtils() {
+    }
+
+    /**
+     * Escapes the given {@code string} so that it's safe to use in HTML.
+     *
+     * @param string If {@code null}, returns an empty string.
+     * @return Never {@code null}.
+     */
+    public static String html(String string) {
+        return string != null ? StringUtils.escapeHtml(string) : "";
+    }
+
+    /**
+     * Escapes the given {@code string} so that it's safe to use in
+     * JavaScript.
+     *
+     * @param string If {@code null}, returns an empty string.
+     * @return Never {@code null}.
+     */
+    public static String js(String string) {
+        return string != null ? StringUtils.escapeJavaScript(string) : "";
     }
 
     public static List<String> listLayouts(Object object, String field) {
