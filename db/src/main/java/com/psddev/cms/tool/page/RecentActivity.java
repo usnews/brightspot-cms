0115218dca5f02fb3009c8abda1c80ae0de097a0
Hyoo Lim <hyoolim@gmail.com>
2013-02-22 17:46:32 -0500

    Adds user and role selection to recent activity widget.

--- db/src/main/java/com/psddev/cms/tool/page/RecentActivity.java
+++ db/src/main/java/com/psddev/cms/tool/page/RecentActivity.java
@@ -1,7 +1,6 @@
 package com.psddev.cms.tool.page;
 
 import java.io.IOException;
-import java.util.Date;
 import java.util.UUID;
 
 import javax.servlet.ServletException;
@@ -11,6 +10,7 @@ import org.joda.time.DateTime;
 import com.psddev.cms.db.Content;
 import com.psddev.cms.db.Directory;
 import com.psddev.cms.db.Template;
+import com.psddev.cms.db.ToolRole;
 import com.psddev.cms.db.ToolUser;
 import com.psddev.cms.tool.PageServlet;
 import com.psddev.cms.tool.PageWriter;
@@ -35,33 +35,58 @@ public class RecentActivity extends PageServlet {
     @Override
     protected void doService(ToolPageContext page) throws IOException, ServletException {
         ObjectType itemType = Query.findById(ObjectType.class, page.pageParam(UUID.class, "itemType", null));
-        DateTime date = new DateTime(page.param(Date.class, "date"));
-        UUID userId = page.pageParam(UUID.class, "userId", null);
+        Type type = page.pageParam(Type.class, "type", Type.EVERYONE);
+        String valueParameter = type + ".value";
+        Object valueObject = Query.from(Object.class).where("_id = ?", page.pageParam(UUID.class, valueParameter, null)).first();
         long offset = page.param(long.class, "offset");
         int limit = page.pageParam(Integer.class, "limit", 20);
 
-        Query<?> contentQuery = (itemType != null ? Query.fromType(itemType) : Query.from(Content.class)).
-                where(page.siteItemsPredicate()).
-                and(Content.UPDATE_DATE_FIELD + " != missing").
-                and(Content.UPDATE_DATE_FIELD + " <= ?", date).
-                sortDescending(Content.UPDATE_DATE_FIELD);
-
-        if (userId != null) {
-            contentQuery.and(Content.UPDATE_USER_FIELD + " = ?", userId);
+        if (type == null) {
+            type = Type.EVERYONE;
         }
 
-        PaginatedResult<?> contents = contentQuery.select(offset, limit);
+        PaginatedResult<?> result;
+
+        if (valueObject == null && (type == Type.ROLE || type == Type.USER)) {
+            result = null;
+
+        } else {
+            Query<?> contentQuery = (itemType != null ? Query.fromType(itemType) : Query.from(Content.class)).
+                    where(page.siteItemsPredicate()).
+                    and(Content.UPDATE_DATE_FIELD + " != missing").
+                    sortDescending(Content.UPDATE_DATE_FIELD);
+
+            switch (type) {
+                case ROLE :
+                    contentQuery.and(Content.UPDATE_USER_FIELD + " = ?", Query.from(ToolUser.class).where("role = ?", valueObject));
+                    break;
+
+                case USER :
+                    contentQuery.and(Content.UPDATE_USER_FIELD + " = ?", valueObject);
+                    break;
+
+                case ME :
+                    contentQuery.and(Content.UPDATE_USER_FIELD + " = ?", page.getUser());
+                    break;
+
+                default :
+                    break;
+            }
+
+            result = contentQuery.select(offset, limit);
+        }
+
         PageWriter writer = page.getWriter();
 
-        writer.start("div", "class", "widget widget-recentActivity");
-            writer.start("h1").html("Recent Activity").end();
+        writer.writeStart("div", "class", "widget widget-recentActivity");
+            writer.writeStart("h1").writeHtml("Recent Activity").writeEnd();
 
-            writer.start("form",
+            writer.writeStart("form",
                     "class", "recentActivity-filters autoSubmit",
                     "method", "get",
                     "action", page.url(null));
 
-                writer.start("span", "class", "recentActivity-filters-itemType");
+                writer.writeStart("span", "class", "recentActivity-filters-itemType");
                     page.typeSelect(
                             Template.Static.findUsedTypes(page.getSite()),
                             itemType,
@@ -69,129 +94,216 @@ public class RecentActivity extends PageServlet {
                             "class", "autoSubmit",
                             "name", "itemType",
                             "data-searchable", "true");
-                writer.end();
+                writer.writeEnd();
 
-                writer.start("span", "class", "recentActivity-filters-user");
-                    writer.start("span", "class", "recentActivity-filters-by");
-                        writer.html("by");
-                    writer.end();
+                writer.writeStart("span", "class", "recentActivity-filters-prep");
+                    writer.writeHtml("by");
+                writer.writeEnd();
 
-                    writer.start("select", "name", "userId");
-                        writer.start("option",
-                                "selected", userId == null ? "selected" : null,
-                                "value", "");
-                            writer.html("Everyone");
-                        writer.end();
-                        writer.start("option",
-                                "selected", userId != null ? "selected" : null,
-                                "value", page.getUser().getId());
-                            writer.html("Me");
-                        writer.end();
-                    writer.end();
-                writer.end();
-
-            writer.end();
-
-            writer.start("ul", "class", "pagination");
-
-                if (contents.hasPrevious()) {
-                    writer.start("li", "class", "first");
-                        writer.start("a",
-                                "href", page.url("", "offset", contents.getFirstOffset()));
-                            writer.html("Newest");
-                        writer.end();
-                    writer.end();
-
-                    writer.start("li", "class", "previous");
-                        writer.start("a",
-                                "href", page.url("", "offset", contents.getPreviousOffset()));
-                            writer.html("Newer ").html(limit);
-                        writer.end();
-                    writer.end();
-                }
-
-                writer.start("li");
-                    writer.start("form",
-                            "class", "autoSubmit",
-                            "method", "get",
-                            "action", page.url(null));
-                        writer.start("select", "name", "limit");
-                            for (int l : LIMITS) {
-                                writer.start("option",
-                                        "value", l,
-                                        "selected", limit == l ? "selected" : null);
-                                    writer.html("Show ");
-                                    writer.html(l);
-                                writer.end();
+                writer.writeStart("span", "class", "recentActivity-filters-type");
+                    writer.writeStart("select", "name", "type");
+                        for (Type t : Type.values()) {
+                            if (t != Type.ROLE || Query.from(ToolRole.class).first() != null) {
+                                writer.writeStart("option",
+                                        "selected", t.equals(type) ? "selected" : null,
+                                        "value", t.name());
+                                    writer.writeHtml(t.getDisplayName());
+                                writer.writeEnd();
                             }
-                        writer.end();
-                    writer.end();
-                writer.end();
+                        }
+                    writer.writeEnd();
+                writer.writeEnd();
 
-                if (contents.hasNext()) {
-                    writer.start("li", "class", "next");
-                        writer.start("a",
-                                "href", page.url("", "offset", contents.getNextOffset()));
-                            writer.html("Older ").html(limit);
-                        writer.end();
-                    writer.end();
-                }
+                writer.writeStart("span", "class", "recentActivity-filters-value");
+                    Query<?> valueQuery;
 
-            writer.end();
+                    if (type == Type.ROLE) {
+                        valueQuery = Query.from(ToolRole.class).sortAscending("name");
 
-            writer.start("table", "class", "links table-striped pageThumbnails").start("tbody");
+                    } else if (type == Type.USER) {
+                        valueQuery = Query.from(ToolUser.class).sortAscending("name");
 
-                String lastDateString = null;
+                    } else {
+                        valueQuery = null;
+                    }
 
-                for (Object content : contents.getItems()) {
-                    State contentState = State.getInstance(content);
-                    String permalink = contentState.as(Directory.ObjectModification.class).getPermalink();
-                    Content.ObjectModification contentData = contentState.as(Content.ObjectModification.class);
-                    DateTime updateDate = new DateTime(contentData.getUpdateDate());
-                    ToolUser updateUser = contentData.getUpdateUser();
-                    String dateString = updateDate.toString("E, MMM d, yyyy");
-                    String statusId = contentData.getStatusId();
+                    if (valueQuery == null) {
+                        writer.writeHtml("\u0020");
 
-                    writer.start("tr", "data-preview-url", permalink);
-                        writer.start("td", "class", "date");
-                            if (!dateString.equals(lastDateString)) {
-                                writer.html(dateString);
-                                lastDateString = dateString;
-                            }
-                        writer.end();
+                    } else {
+                        if (valueQuery.hasMoreThan(250)) {
+                            State valueState = State.getInstance(valueObject);
 
-                        writer.start("td", "class", "time");
-                            writer.html(updateDate.toString("hh:mm a"));
-                        writer.end();
+                            writer.writeTag("input",
+                                    "type", "text",
+                                    "class", "autoSubmit objectId",
+                                    "data-editable", false,
+                                    "data-label", valueState != null ? valueState.getLabel() : null,
+                                    "data-typeIds", ObjectType.getInstance(ToolRole.class).getId(),
+                                    "name", valueParameter,
+                                    "value", valueState != null ? valueState.getId() : null);
 
-                        writer.start("td");
-                            writer.typeLabel(content);
-                        writer.end();
+                        } else {
+                            writer.writeStart("select",
+                                    "class", "autoSubmit",
+                                    "name", valueParameter,
+                                    "data-searchable", "true");
 
-                        writer.start("td", "data-preview-anchor", "");
-                            if (statusId != null) {
-                                writer.start("span", "class", "contentStatusLabel contentStatusLabel-" + statusId);
-                                    writer.html(contentData.getStatus());
-                                writer.end();
-                            }
+                                writer.writeStart("option", "value", "").writeEnd();
 
-                            writer.html(" ");
+                                for (Object v : valueQuery.selectAll()) {
+                                    State state = State.getInstance(v);
 
-                            writer.start("a",
-                                    "href", page.objectUrl("/content/edit.jsp", content),
-                                    "target", "_top");
-                                writer.objectLabel(content);
-                            writer.end();
-                        writer.end();
+                                    writer.writeStart("option",
+                                            "value", state.getId(),
+                                            "selected", v.equals(valueObject) ? "selected" : null);
+                                        writer.writeHtml(state.getLabel());
+                                    writer.writeEnd();
+                                }
 
-                        writer.start("td");
-                            writer.objectLabel(updateUser);
-                        writer.end();
-                    writer.end();
-                }
+                            writer.writeEnd();
+                        }
+                    }
+                writer.writeEnd();
 
-            writer.end().end();
+            writer.writeEnd();
 
-        writer.end();
+            if (result == null) {
+                writer.writeStart("div", "class", "recentActivity-warning recentActivity-warning-value");
+                    writer.writeStart("p");
+                        writer.writeHtml("Please select a ");
+                        writer.writeHtml(type.getDisplayName());
+                        writer.writeHtml(".");
+                    writer.writeEnd();
+                writer.writeEnd();
+
+            } else if (!result.hasItems()) {
+                writer.writeStart("div", "class", "recentActivity-warning");
+                    writer.writeStart("p");
+                        writer.writeHtml("No recent activity!");
+                    writer.writeEnd();
+                writer.writeEnd();
+
+            } else {
+                writer.writeStart("ul", "class", "pagination");
+
+                    if (result.hasPrevious()) {
+                        writer.writeStart("li", "class", "first");
+                            writer.writeStart("a",
+                                    "href", page.url("", "offset", result.getFirstOffset()));
+                                writer.writeHtml("Newest");
+                            writer.writeEnd();
+                        writer.writeEnd();
+
+                        writer.writeStart("li", "class", "previous");
+                            writer.writeStart("a",
+                                    "href", page.url("", "offset", result.getPreviousOffset()));
+                                writer.writeHtml("Newer ").writeHtml(limit);
+                            writer.writeEnd();
+                        writer.writeEnd();
+                    }
+
+                    writer.writeStart("li");
+                        writer.writeStart("form",
+                                "class", "autoSubmit",
+                                "method", "get",
+                                "action", page.url(null));
+                            writer.writeStart("select", "name", "limit");
+                                for (int l : LIMITS) {
+                                    writer.writeStart("option",
+                                            "value", l,
+                                            "selected", limit == l ? "selected" : null);
+                                        writer.writeHtml("Show ");
+                                        writer.writeHtml(l);
+                                    writer.writeEnd();
+                                }
+                            writer.writeEnd();
+                        writer.writeEnd();
+                    writer.writeEnd();
+
+                    if (result.hasNext()) {
+                        writer.writeStart("li", "class", "next");
+                            writer.writeStart("a",
+                                    "href", page.url("", "offset", result.getNextOffset()));
+                                writer.writeHtml("Older ").writeHtml(limit);
+                            writer.writeEnd();
+                        writer.writeEnd();
+                    }
+
+                writer.writeEnd();
+
+                writer.writeStart("table", "class", "links table-striped pageThumbnails").writeStart("tbody");
+
+                    String lastDateString = null;
+
+                    for (Object content : result.getItems()) {
+                        State contentState = State.getInstance(content);
+                        String permalink = contentState.as(Directory.ObjectModification.class).getPermalink();
+                        Content.ObjectModification contentData = contentState.as(Content.ObjectModification.class);
+                        DateTime updateDate = new DateTime(contentData.getUpdateDate());
+                        ToolUser updateUser = contentData.getUpdateUser();
+                        String dateString = updateDate.toString("E, MMM d, yyyy");
+                        String statusId = contentData.getStatusId();
+
+                        writer.writeStart("tr", "data-preview-url", permalink);
+                            writer.writeStart("td", "class", "date");
+                                if (!dateString.equals(lastDateString)) {
+                                    writer.writeHtml(dateString);
+                                    lastDateString = dateString;
+                                }
+                            writer.writeEnd();
+
+                            writer.writeStart("td", "class", "time");
+                                writer.writeHtml(updateDate.toString("hh:mm a"));
+                            writer.writeEnd();
+
+                            writer.writeStart("td");
+                                writer.typeLabel(content);
+                            writer.writeEnd();
+
+                            writer.writeStart("td", "data-preview-anchor", "");
+                                if (statusId != null) {
+                                    writer.writeStart("span", "class", "contentStatusLabel contentStatusLabel-" + statusId);
+                                        writer.writeHtml(contentData.getStatus());
+                                    writer.writeEnd();
+                                }
+
+                                writer.writeHtml(" ");
+
+                                writer.writeStart("a",
+                                        "href", page.objectUrl("/content/edit.jsp", content),
+                                        "target", "_top");
+                                    writer.objectLabel(content);
+                                writer.writeEnd();
+                            writer.writeEnd();
+
+                            writer.writeStart("td");
+                                writer.objectLabel(updateUser);
+                            writer.writeEnd();
+                        writer.writeEnd();
+                    }
+
+                writer.writeEnd().writeEnd();
+            }
+
+        writer.writeEnd();
+    }
+
+    private enum Type {
+
+        EVERYONE("Everyone"),
+        ME("Me"),
+        ROLE("Role"),
+        USER("User");
+
+        private String displayName;
+
+        private Type(String displayName) {
+            this.displayName = displayName;
+        }
+
+        public String getDisplayName() {
+            return displayName;
+        }
     }
 }
