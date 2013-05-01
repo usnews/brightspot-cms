523f4a7494dae513c9637badbcb47df190edc77e
Hyoo Lim <hyoolim@gmail.com>
2013-04-30 22:39:16 -0400

    In Schedule, adds @Indexed to name, adds @ToolUi.Hidden to triggerSite and triggerUser, and cleans up the trigger method.

--- db/src/main/java/com/psddev/cms/db/Schedule.java
+++ db/src/main/java/com/psddev/cms/db/Schedule.java
@@ -1,21 +1,28 @@
 package com.psddev.cms.db;
 
-import com.psddev.dari.db.Record;
-import com.psddev.dari.db.Query;
-import com.psddev.dari.db.State;
-
 import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.psddev.dari.db.Query;
+import com.psddev.dari.db.Record;
+import com.psddev.dari.util.ObjectUtils;
+
 public class Schedule extends Record {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Schedule.class);
 
+    @Indexed(unique = true)
     private String name;
-    private @Indexed Date triggerDate;
+
+    @Indexed
+    private Date triggerDate;
+
+    @ToolUi.Hidden
     private Site triggerSite;
+
+    @ToolUi.Hidden
     private ToolUser triggerUser;
 
     public String getName() {
@@ -62,24 +69,39 @@ public class Schedule extends Record {
         }
 
         LOGGER.info("Triggering [{}] schedule", getLabel());
+
         try {
             beginWrites();
-            for (Draft draft : Query
-                    .from(Draft.class)
-                    .where("schedule = ?", this)
-                    .selectAll()) {
-                LOGGER.debug(
-                        "Processing [{}] draft in [{}] schedule",
-                        draft.getLabel(), getLabel());
+
+            for (Draft draft : Query.
+                    from(Draft.class).
+                    where("schedule = ?", this).
+                    selectAll()) {
+                LOGGER.debug("Processing [{}] draft in [{}] schedule", draft.getLabel(), getLabel());
+
                 Object object = draft.getObject();
+
                 if (object != null) {
-                    Content.Static.publish(object, getTriggerSite(), getTriggerUser());
+                    ToolUser triggerUser = getTriggerUser();
+
+                    if (triggerUser == null) {
+                        triggerUser = draft.as(Content.ObjectModification.class).getUpdateUser();
+
+                        if (triggerUser == null) {
+                            triggerUser = draft.getOwner();
+                        }
+                    }
+
+                    Content.Static.publish(object, getTriggerSite(), triggerUser);
                 }
+
                 draft.delete();
             }
+
             delete();
             commitWrites();
             return true;
+
         } finally {
             endWrites();
         }
