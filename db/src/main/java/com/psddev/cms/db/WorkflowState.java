b7a621dec1010a221863c53a6347cb63910c4dd9
Hyoo Lim <hyoolim@gmail.com>
2013-05-07 20:14:01 -0400

    Adds #equals, #hashCode, and #toString to WorkflowState class.

--- db/src/main/java/com/psddev/cms/db/WorkflowState.java
+++ db/src/main/java/com/psddev/cms/db/WorkflowState.java
@@ -1,5 +1,7 @@
 package com.psddev.cms.db;
 
+import com.psddev.dari.util.ObjectUtils;
+
 public class WorkflowState {
 
     private String name;
@@ -10,5 +12,28 @@ public class WorkflowState {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+
+        } else if (other instanceof WorkflowState) {
+            return ObjectUtils.equals(getName(), ((WorkflowState) other).getName());
+
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return ObjectUtils.hashCode(getName());
+    }
+
+    @Override
+    public String toString() {
+        return getName();
     }
 }
