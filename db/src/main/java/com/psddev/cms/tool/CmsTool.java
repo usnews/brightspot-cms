741501a03a0193dc237ac3953ba446eefd26234a
Hyoo Lim <hyoolim@gmail.com>
2013-05-18 12:10:08 -0400

    Removes template and URLs area.

--- db/src/main/java/com/psddev/cms/tool/CmsTool.java
+++ db/src/main/java/com/psddev/cms/tool/CmsTool.java
@@ -414,9 +414,7 @@ public class CmsTool extends Tool {
         plugins.add(createArea2("Production Guides", "adminGuides", "admin/adminGuides", "/admin/guides.jsp"));
         plugins.add(createArea2("Settings", "adminSettings", "admin/adminSettings", "/admin/settings.jsp"));
         plugins.add(createArea2("Sites", "adminSites", "admin/adminSites", "/admin/sites.jsp"));
-        plugins.add(createArea2("Templates & Sections", "adminTemplates", "admin/adminTemplates", "/admin/templates.jsp"));
         plugins.add(createArea2("Trash", "cms.adminTrash", "admin/trash", "/adminTrash"));
-        plugins.add(createArea2("URLs", "adminUrls", "admin/adminUrls", "/admin/urls.jsp"));
         plugins.add(createArea2("Users & Roles", "adminUsers", "admin/adminUsers", "/admin/users.jsp"));
         plugins.add(createArea2("Variations & Profiles", "adminVariations", "admin/adminVariations", "/admin/variations.jsp"));
         plugins.add(createArea2("Workflows", "adminWorkflows", "admin/adminWorkflows", "/admin/workflows.jsp"));
