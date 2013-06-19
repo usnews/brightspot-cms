0e60c8d4d51029707cb681da6f169dbbf21b9af3
Hyoo Lim <hyoolim@gmail.com>
2013-06-19 13:37:50 -0400

    Improves Head and HeadNode to remove the possibility of duplicate tags.

--- db/src/main/java/com/psddev/cms/db/Head.java
+++ db/src/main/java/com/psddev/cms/db/Head.java
@@ -3,16 +3,24 @@ package com.psddev.cms.db;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import com.psddev.dari.util.ErrorUtils;
 import com.psddev.dari.util.HtmlWriter;
 import com.psddev.dari.util.ObjectUtils;
+import com.psddev.dari.util.TypeDefinition;
 
+/**
+ * Collection of all nodes within the {@code <head>} element.
+ */
 public class Head {
 
     private List<HeadNode> nodes;
 
     /**
+     * Returns the list of all nodes in the {@code <head>} element.
+     *
      * @return Never {@code null}. Mutable.
      */
     public List<HeadNode> getNodes() {
@@ -23,74 +31,180 @@ public class Head {
     }
 
     /**
+     * Sets the list of all nodes in the {@code <head>} element.
+     *
      * @param nodes May be {@code null} to clear the list.
      */
     public void setNodes(List<HeadNode> nodes) {
         this.nodes = nodes;
     }
 
+    @SuppressWarnings("unchecked")
+    private <T extends HeadNode.Tag> T findOrCreate(Class<T> tagClass, String name, Object... attributes) {
+        ErrorUtils.errorIfBlank(name, "name");
+
+        for (HeadNode node : getNodes()) {
+            if (!node.getClass().equals(tagClass)) {
+                continue;
+            }
+
+            HeadNode.Tag tag = (HeadNode.Tag) node;
+
+            if (name.equals(tag.getName()) &&
+                    tag.hasAttributes(attributes)) {
+                return (T) tag;
+            }
+        }
+
+        HeadNode.Tag tag = TypeDefinition.getInstance(tagClass).newInstance();
+
+        tag.setName(name);
+        tag.addAttributes(attributes);
+        getNodes().add(tag);
+        return (T) tag;
+    }
+
     /**
-     * @param tagName If blank, does nothing.
+     * Finds or creates a self-closing tag with the given {@code name} and
+     * {@code attributes}.
+     *
+     * @param name Can't be blank.
      * @param attributes May be {@code null}.
+     * @return Never {@code null}.
      */
-    public void addTag(String tagName, Object... attributes) {
-        if (!ObjectUtils.isBlank(tagName)) {
-            getNodes().add(HeadNode.tag(tagName, attributes));
+    public HeadNode.Tag findOrCreateTag(String name, Object... attributes) {
+        return findOrCreate(HeadNode.Tag.class, name, attributes);
+    }
+
+    /**
+     * Finds or creates an element with the given {@code name} and
+     * {@code attributes}.
+     *
+     * @param name Can't be blank.
+     * @param attributes May be {@code null}.
+     * @return Never {@code null}.
+     */
+    public HeadNode.Element findOrCreateElement(String name, Object... attributes) {
+        return findOrCreate(HeadNode.Element.class, name, attributes);
+    }
+
+    /**
+     * Removes all tags with the given {@code name} and {@code attributes}.
+     *
+     * @param name If blank, does nothing.
+     * @param attribues May be {@code null}.
+     */
+    public void removeTag(String name, Object... attributes) {
+        if (ObjectUtils.isBlank(name)) {
+            return;
+        }
+
+        for (Iterator<HeadNode> i = getNodes().iterator(); i.hasNext(); ) {
+            HeadNode node = i.next();
+
+            if (!(node instanceof HeadNode.Tag)) {
+                continue;
+            }
+
+            HeadNode.Tag tag = (HeadNode.Tag) node;
+
+            if (name.equals(tag.getName()) &&
+                    tag.hasAttributes(attributes)) {
+                i.remove();
+            }
         }
     }
 
     /**
-     * @param title If blank, does nothing.
+     * Sets {@code <title>...</title>}.
+     *
+     * @param title If blank, removes the element.
      */
     public void setTitle(String title) {
-        if (!ObjectUtils.isBlank(title)) {
-            getNodes().add(HeadNode.element("title").
-                    child(HeadNode.text(title)));
+        if (ObjectUtils.isBlank(title)) {
+            removeTag("title");
+
+        } else {
+            List<HeadNode> children = findOrCreateElement("title").getChildren();
+
+            children.clear();
+            children.add(HeadNode.text(title));
         }
     }
 
     /**
+     * Sets {@code <link rel="canonical" href="...">}.
+     *
+     * @param url If blank, removes the tag.
+     */
+    public void setCanonicalUrl(String url) {
+        if (ObjectUtils.isBlank(url)) {
+            removeTag("link", "rel", "canonical");
+
+        } else {
+            findOrCreateTag("link", "rel", "canonical").getAttributes().put("href", url);
+        }
+    }
+
+    /**
+     * Sets {@code <meta name="..." content="...">}.
+     *
      * @param name If blank, does nothing.
-     * @param content May be {@code null}.
+     * @param content If blank, removes the tag.
      */
-    public void addMetaName(String name, String content) {
+    public void setMetaName(String name, String content) {
         if (!ObjectUtils.isBlank(name)) {
-            addTag("meta",
-                    "name", name,
-                    "content", content);
+            if (ObjectUtils.isBlank(content)) {
+                removeTag("meta", "name", name);
+
+            } else {
+                findOrCreateTag("meta", "name", name).getAttributes().put("content", content);
+            }
         }
     }
 
     /**
+     * Sets {@code <meta property="..." content="...">}.
+     *
      * @param property If blank, does nothing.
-     * @param content May be {@code null}.
+     * @param content If blank, removes the tag.
      */
-    public void addMetaProperty(String property, String content) {
+    public void setMetaProperty(String property, String content) {
         if (!ObjectUtils.isBlank(property)) {
-            addTag("meta",
-                    "property", property,
-                    "content", content);
+            if (ObjectUtils.isBlank(content)) {
+                removeTag("meta", "property", property);
+
+            } else {
+                findOrCreateTag("meta", "property", property).getAttributes().put("content", content);
+            }
         }
     }
 
     /**
+     * Sets {@code <meta http-equiv="..." content="...">}.
+     *
      * @param httpEquiv If blank, does nothing.
-     * @param content May be {@code null}.
+     * @param content If blank, removes the tag.
      */
-    public void addHttpEquiv(String httpEquiv, String content) {
+    public void setHttpEquiv(String httpEquiv, String content) {
         if (!ObjectUtils.isBlank(httpEquiv)) {
-            addTag("meta",
-                    "http-equiv", httpEquiv,
-                    "content", content);
+            if (ObjectUtils.isBlank(content)) {
+                removeTag("meta", "http-equiv", httpEquiv);
+
+            } else {
+                findOrCreateTag("meta", "http-equiv", httpEquiv).getAttributes().put("contnet", content);
+            }
         }
     }
 
     /**
+     * Adds {@code <link rel="stylsheet" type="text/css" href="...">}.
+     *
      * @param href If blank, does nothing.
      */
     public void addStyleSheet(String href) {
         if (!ObjectUtils.isBlank(href)) {
-            addTag("link",
+            findOrCreateTag("link",
                     "rel", href.endsWith(".less") ? "stylesheet/less" : "stylesheet",
                     "type", "text/css",
                     "href", ElFunctionUtils.resource(href));
@@ -98,16 +212,21 @@ public class Head {
     }
 
     /**
+     * Adds {@code <script type="text/javascript" src="..."></script>}.
+     *
      * @param src If blank, does nothing.
      */
     public void addScript(String src) {
         if (!ObjectUtils.isBlank(src)) {
-            getNodes().add(HeadNode.element("script",
+            findOrCreateElement("script",
                     "type", "text/javascript",
-                    "src", ElFunctionUtils.resource(src)));
+                    "src", ElFunctionUtils.resource(src));
         }
     }
 
+    /**
+     * Writes the contents of the {@code <head>} to the given {@code writer}.
+     */
     public void writeHtml(HtmlWriter writer) throws IOException {
         for (HeadNode node : getNodes()) {
             node.writeHtml(writer);
--- db/src/main/java/com/psddev/cms/db/HeadNode.java
+++ db/src/main/java/com/psddev/cms/db/HeadNode.java
@@ -3,25 +3,70 @@ package com.psddev.cms.db;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
+import com.psddev.dari.util.CompactMap;
+import com.psddev.dari.util.ErrorUtils;
 import com.psddev.dari.util.HtmlWriter;
 
+/**
+ * Node within the {@code <head>} element.
+ *
+ * @see Head
+ */
 public abstract class HeadNode {
 
-    public static HeadNode.Tag tag(String tagName, Object... attributes) {
-        return new Tag(tagName, attributes);
+    /**
+     * Creates a self-closing tag with the given {@code name} and
+     * {@code attributes}.
+     *
+     * @param name Can't be blank.
+     * @param attributes May be {@code null}.
+     */
+    public static HeadNode.Tag tag(String name, Object... attributes) {
+        ErrorUtils.errorIfBlank(name, "name");
+
+        Tag tag = new Tag();
+
+        tag.setName(name);
+        tag.addAttributes(attributes);
+        return tag;
     }
 
-    public static HeadNode.Element element(String tagName, Object... attributes) {
-        return new Element(tagName, attributes);
+    /**
+     * Creates an element with the given {@code name} and {@code attributes}.
+     *
+     * @param name Can't be blank.
+     * @param attributes May be {@code null}.
+     */
+    public static HeadNode.Element element(String name, Object... attributes) {
+        ErrorUtils.errorIfBlank(name, "name");
+
+        Element element = new Element();
+
+        element.setName(name);
+        element.addAttributes(attributes);
+        return element;
     }
 
+    /**
+     * Creates a text node with the given {@code text}.
+     *
+     * @param text If {@code null}, creates a text node with an empty string.
+     */
     public static HeadNode.Text text(String text) {
-        return new Text(text);
+        Text textNode = new Text();
+
+        textNode.setText(text != null ? text : "");
+        return textNode;
     }
 
+    /**
+     * Writes this node as HTML to the given {@code writer}.
+     *
+     * @param writer Can't be {@code null}.
+     */
     public abstract void writeHtml(HtmlWriter writer) throws IOException;
 
     @Override
@@ -44,45 +89,178 @@ public abstract class HeadNode {
         return string.toString();
     }
 
+    /**
+     * Self-closing tag within the {@code <head>}, such as {@code <link>}.
+     */
     public static class Tag extends HeadNode {
 
-        private final String tagName;
-        private final Object[] attributes;
+        private String name;
+        private Map<String, String> attributes;
 
-        private Tag(String tagName, Object... attributes) {
-            this.tagName = tagName;
-            this.attributes = attributes != null ? Arrays.copyOf(attributes, attributes.length) : new Object[0];
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        /**
+         * @return Never {@code null}. Mutable.
+         */
+        public Map<String, String> getAttributes() {
+            if (attributes == null) {
+                attributes = new CompactMap<String, String>();
+            }
+            return attributes;
+        }
+
+        /**
+         * @param attributes May be {@code null} to clear the map.
+         */
+        public void setAttributes(Map<String, String> attributes) {
+            this.attributes = attributes;
+        }
+
+        /**
+         * Adds all given {@code attributes}.
+         *
+         * @param attributes May be {@code null}.
+         */
+        public void addAttributes(Object... attributes) {
+            if (attributes == null) {
+                return;
+            }
+
+            for (int i = 0, length = attributes.length; i < length; ++ i) {
+                Object name = attributes[i];
+
+                if (name == null) {
+                    ++ i;
+
+                } else if (name instanceof Map) {
+                    for (Map.Entry<?, ?> entry : ((Map<?, ?>) name).entrySet()) {
+                        Object n = entry.getKey();
+                        Object v = entry.getValue();
+
+                        if (n != null && v != null) {
+                            getAttributes().put(n.toString(), v.toString());
+                        }
+                    }
+
+                } else {
+                    ++ i;
+
+                    if (i < length) {
+                        Object value = attributes[i];
+
+                        if (value != null) {
+                            getAttributes().put(name.toString(), value.toString());
+                        }
+                    }
+                }
+            }
+        }
+
+        /**
+         * Returns {@code true} if this tag contains the given
+         * {@code attributes}.
+         *
+         * @param attributes If {@code null}, always returns {@code true}.
+         */
+        public boolean hasAttributes(Object... attributes) {
+            if (attributes == null) {
+                return true;
+            }
+
+            int length = attributes.length;
+
+            if (length == 0) {
+                return true;
+            }
+
+            for (int i = 0; i < length; ++ i) {
+                Object name = attributes[i];
+
+                if (name == null) {
+                    throw new IllegalArgumentException();
+
+                } else if (name instanceof Map) {
+                    for (Map.Entry<?, ?> entry : ((Map<?, ?>) name).entrySet()) {
+                        Object n = entry.getKey();
+
+                        if (n == null) {
+                            throw new IllegalArgumentException();
+                        }
+
+                        Object v = entry.getValue();
+
+                        if (v == null) {
+                            throw new IllegalArgumentException();
+                        }
+
+                        if (!v.toString().equals(getAttributes().get(n.toString()))) {
+                            return false;
+                        }
+                    }
+
+                } else {
+                    ++ i;
+
+                    if (i >= length) {
+                        throw new IllegalArgumentException();
+                    }
+
+                    Object value = attributes[i];
+
+                    if (value == null) {
+                        throw new IllegalArgumentException();
+                    }
+
+                    if (!value.toString().equals(getAttributes().get(name.toString()))) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
         }
 
         @Override
         public void writeHtml(HtmlWriter writer) throws IOException {
-            writer.writeTag(tagName, attributes);
+            writer.writeTag(getName(), getAttributes());
         }
     }
 
-    public static class Element extends HeadNode {
+    /**
+     * Element within the {@code <head>}, such as {@code <title>}.
+     */
+    public static class Element extends HeadNode.Tag {
 
-        private final String tagName;
-        private final Object[] attributes;
         private List<HeadNode> children;
 
-        private Element(String tagName, Object... attributes) {
-            this.tagName = tagName;
-            this.attributes = attributes != null ? Arrays.copyOf(attributes, attributes.length) : new Object[0];
-        }
-
-        public HeadNode child(HeadNode child) {
+        /**
+         * @return Never {@code null}. Mutable.
+         */
+        public List<HeadNode> getChildren() {
             if (children == null) {
                 children = new ArrayList<HeadNode>();
             }
+            return children;
+        }
 
-            children.add(child);
-            return this;
+        /**
+         * @param children May be {@code null} to clear the list.
+         */
+        public void setChildren(List<HeadNode> children) {
+            this.children = children;
         }
 
         @Override
         public void writeHtml(HtmlWriter writer) throws IOException {
-            writer.writeStart(tagName, attributes);
+            writer.writeStart(getName(), getAttributes());
+
+            List<HeadNode> children = getChildren();
 
             if (children != null) {
                 for (HeadNode child : children) {
@@ -94,11 +272,19 @@ public abstract class HeadNode {
         }
     }
 
+    /**
+     * Text within the {@code <head>}, typically as a child in an
+     * {@link Element}.
+     */
     public static class Text extends HeadNode {
 
-        private final String text;
+        private String text;
 
-        private Text(String text) {
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
             this.text = text;
         }
 
