b9c95cb52cb426b238f996653408d9db1d56670f
Lee Teague <lee@perfectsensedigital.com>
2013-10-02 12:47:21 -0500

    Formatting

--- db/src/main/java/com/psddev/cms/db/VideoTranscodingStatusUpdateTask.java
+++ db/src/main/java/com/psddev/cms/db/VideoTranscodingStatusUpdateTask.java
@@ -12,57 +12,57 @@ import com.psddev.dari.util.StorageItem;
 import com.psddev.dari.util.Task;
 
 public  class VideoTranscodingStatusUpdateTask extends Task {
-	private static final Logger LOGGER = LoggerFactory.getLogger(VideoTranscodingStatusUpdateTask.class);
-	private static final String DEFAULT_TASK_NAME = "Video Transcoding Status Update Task";
+    private static final Logger LOGGER = LoggerFactory.getLogger(VideoTranscodingStatusUpdateTask.class);
+    private static final String DEFAULT_TASK_NAME = "Video Transcoding Status Update Task";
 
-	public VideoTranscodingStatusUpdateTask(String executor, String name) {
-		super(executor, name);
-	}
+    public VideoTranscodingStatusUpdateTask(String executor, String name) {
+        super(executor, name);
+    }
 
-	public VideoTranscodingStatusUpdateTask(String name) {
-		super(null, name);
-	}
+    public VideoTranscodingStatusUpdateTask(String name) {
+        super(null, name);
+    }
 
-	public VideoTranscodingStatusUpdateTask() {
-		super(null, DEFAULT_TASK_NAME);
-	}
-        @Override
-        protected void doTask() throws Exception {
-		LOGGER.info("VideoTranscodingStatusUpdateTask starting...");
-		this.setProgress("Starting ....");
-		List<VideoContainer> pendingVideoList = Query
-		        .from(VideoContainer.class)
-		        .where("cms.video.transcodingStatus = 'pending' ").selectAll();
-		this.setProgress("Identified pending videos. Pending video count ...."
-		        + pendingVideoList.size());
-		for (VideoContainer video : pendingVideoList) {
-			try {
-				VideoContainer.VideoContainerModification videoContainerModification = video
-				        .as(VideoContainer.VideoContainerModification.class);
-				StorageItem videoStorage = videoContainerModification.getFile();
-				if (videoStorage instanceof KalturaStorageItem) {
-					KalturaStorageItem kalturaStorageItem = (KalturaStorageItem) videoStorage;
-					kalturaStorageItem.pull();
-					KalturaEntryStatus kalturaEntryStatus = kalturaStorageItem.getStatus();  
-					// If transcoding is complete..update status on the video
-					// and save..
-					if (kalturaEntryStatus.equals(com.kaltura.client.enums.KalturaEntryStatus.READY))
-				        {
-						videoContainerModification.setTranscodingStatus(VideoContainer.TranscodingStatus.SUCCEEDED);
-						videoContainerModification.save();
-					} else if (kalturaEntryStatus.equals(KalturaEntryStatus.ERROR_CONVERTING) ||
-							   kalturaEntryStatus.equals(KalturaEntryStatus.SCAN_FAILURE) ||
-							   kalturaEntryStatus.equals(KalturaEntryStatus.INFECTED) ||
-							   kalturaEntryStatus.equals(KalturaEntryStatus.NO_CONTENT) ) {
-						videoContainerModification.setTranscodingStatus(VideoContainer.TranscodingStatus.FAILED);
-						videoContainerModification.save();
-					}
-				}
-			} catch (Exception e) {
-				LOGGER.error("Transcoding status update failed for Video :"+ video.toString(), e);
-			}
-		}
-		this.setProgress("All Done ....");
+    public VideoTranscodingStatusUpdateTask() {
+        super(null, DEFAULT_TASK_NAME);
+    }
+
+    @Override
+    protected void doTask() throws Exception {
+        LOGGER.info("VideoTranscodingStatusUpdateTask starting...");
+        this.setProgress("Starting ....");
+        List<VideoContainer> pendingVideoList = Query
+                .from(VideoContainer.class)
+                .where("cms.video.transcodingStatus = 'pending' ").selectAll();
+        this.setProgress("Identified pending videos. Pending video count ...."
+                + pendingVideoList.size());
+        for (VideoContainer video : pendingVideoList) {
+            try {
+                VideoContainer.Data videoContainerModification = video
+                        .as(VideoContainer.Data.class);
+                StorageItem videoStorage = videoContainerModification.getFile();
+                if (videoStorage instanceof KalturaStorageItem) {
+                    KalturaStorageItem kalturaStorageItem = (KalturaStorageItem) videoStorage;
+                    kalturaStorageItem.pull();
+                    KalturaEntryStatus kalturaEntryStatus = kalturaStorageItem.getStatus();  
+                    // If transcoding is complete..update status on the video
+                    // and save..
+                    if (kalturaEntryStatus.equals(KalturaEntryStatus.READY))
+                        {
+                        videoContainerModification.setTranscodingStatus(VideoContainer.TranscodingStatus.SUCCEEDED);
+                        videoContainerModification.save();
+                    } else if (kalturaEntryStatus.equals(KalturaEntryStatus.ERROR_CONVERTING) ||
+                               kalturaEntryStatus.equals(KalturaEntryStatus.SCAN_FAILURE) ||
+                               kalturaEntryStatus.equals(KalturaEntryStatus.INFECTED) ||
+                               kalturaEntryStatus.equals(KalturaEntryStatus.NO_CONTENT) ) {
+                        videoContainerModification.setTranscodingStatus(VideoContainer.TranscodingStatus.FAILED);
+                        videoContainerModification.save();
+                    }
+                }
+            } catch (Exception e) {
+                LOGGER.error("Transcoding status update failed for Video :"+ video.toString(), e);
+            }
         }
+        this.setProgress("All Done ....");
+    }
 }
-
