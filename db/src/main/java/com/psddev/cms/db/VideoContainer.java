0be1f92675c00a35a00dc1b3b4b9aaa253f3f4e5
Lee Teague <lee@perfectsensedigital.com>
2013-10-02 12:46:37 -0500

    Adds TypeModification and logic to find first VideoStorageItem field

--- db/src/main/java/com/psddev/cms/db/VideoContainer.java
+++ db/src/main/java/com/psddev/cms/db/VideoContainer.java
@@ -1,62 +1,85 @@
 package com.psddev.cms.db; 
 import java.util.Map;
 
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+
 import com.psddev.dari.db.Modification;
+import com.psddev.dari.db.ObjectField;
+import com.psddev.dari.db.ObjectType;
 import com.psddev.dari.db.Recordable;
-import com.psddev.dari.util.StorageItem;
-import com.psddev.cms.db.ToolUi;
+import com.psddev.dari.util.VideoStorageItem;
 
 /**
 * Interface used to add VideContainer Modification 
 */
 public interface VideoContainer extends Recordable {
     
-    public  enum  TranscodingStatus {
+    public enum TranscodingStatus {
             PENDING,
             SUCCEEDED,
             FAILED
-        }
+    }
+
     /**
     * Modification which add information specific to video
     */
     @Modification.FieldInternalNamePrefix("cms.video.")
-    public static final class VideoContainerModification extends Modification<VideoContainer> {
-        //private Map<String, Object> metadata;
+    public static final class Data extends Modification<VideoContainer> {
 
-	@Indexed
-	private TranscodingStatus transcodingStatus;
-	
-	public TranscodingStatus getTranscodingStatus() {
-		return transcodingStatus;
-	}
-	public void setTranscodingStatus(TranscodingStatus transcodingStatus) {
-		this.transcodingStatus = transcodingStatus;
-	}
-        @Required
-        @ToolUi.Note("Required Field")
-        private StorageItem file;
+        //private static final Logger LOGGER = LoggerFactory.getLogger(Data.class);
+
+        @Indexed
+        private TranscodingStatus transcodingStatus;
         
-	public StorageItem getFile() {
-		return file;
-	}
+        public TranscodingStatus getTranscodingStatus() {
+            return transcodingStatus;
+        }
 
-	public void setFile(StorageItem file) {
-		this.file = file;
-	}
+        public void setTranscodingStatus(TranscodingStatus transcodingStatus) {
+            this.transcodingStatus = transcodingStatus;
+        }
+
+        public ObjectField getVideoStorageItemField() {
+            return getState().getType().as(TypeModification.class).getVideoStorageItemField();
+        }
+
+        public VideoStorageItem getFile() {
+            return (getVideoStorageItemField() == null ? null : (VideoStorageItem) getState().getByPath(getVideoStorageItemField().getInternalName()));
+        }
 
         /** Sets the collection of metadata. */
         public void setMetadata(Map<String, Object> metadata) {
-             file.setMetadata(metadata);
+            getFile().setMetadata(metadata);
         }
 
         public Map<String, Object> getMetadata() {
-            return file.getMetadata();
+            return getFile().getMetadata();
         }
 
         @Override
         public void beforeSave() {
             if (transcodingStatus == null)
-              transcodingStatus=TranscodingStatus.PENDING;
+                transcodingStatus=TranscodingStatus.PENDING;
+        }
+    }
+
+    public static final class TypeModification extends Modification<ObjectType> {
+        //private static final Logger LOGGER = LoggerFactory.getLogger(TypeModification.class);
+
+        private ObjectField videoStorageItemField;
+
+        public ObjectField getVideoStorageItemField() {
+            videoStorageItemField = null;
+            if (videoStorageItemField == null) {
+                for (ObjectField field : getOriginalObject().getFields()) {
+                    if (VideoStorageItem.class.isAssignableFrom(field.getJavaField(getOriginalObject().getObjectClass()).getType())) {
+                        videoStorageItemField = field;
+                        break;
+                    }
+                }
+            }
+            return videoStorageItemField;
         }
     }
 
