c5dd89fdf2bdfb4d96f0e6c020303afee30ee944
Hyoo Lim <hyoolim@gmail.com>
2013-10-31 08:44:51 -0400

    Fixes enhancement overlay in RTE to auto-expand to fit the content.

--- tool-ui/src/main/webapp/script/jquery.rte.js
+++ tool-ui/src/main/webapp/script/jquery.rte.js
@@ -1275,6 +1275,8 @@ var Rte = wysihtml5.Editor.extend({
             $.data($enhancement[0], 'rte-visited', true);
 
             // Position the enhancement to cover the placeholder.
+            $placeholder.css('padding-top', $enhancement.find('> .rte-toolbar').outerHeight() - 42);
+
             $enhancement.css({
                 'height': $placeholder.outerHeight(),
                 'left': placeholderOffset.left,
