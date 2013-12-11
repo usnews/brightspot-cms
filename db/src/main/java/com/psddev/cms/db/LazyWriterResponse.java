7fcd9d89b6ec314e2b60831cfbc9d2babc1da3d9
Hyoo Lim <hyoolim@gmail.com>
2013-12-11 15:05:31 -0500

    Deprecates LazyWriter and LazyWriterResponse (use the ones in dari-util instead).

--- db/src/main/java/com/psddev/cms/db/LazyWriterResponse.java
+++ db/src/main/java/com/psddev/cms/db/LazyWriterResponse.java
@@ -1,38 +1,16 @@
 package com.psddev.cms.db;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpServletResponseWrapper;
 
 /**
  * {@link HttpServletResponse} that uses the {@link LazyWriter}.
+ *
+ * @deprecated Use {@link com.psddev.dari.util.LazyWriterResponse} instead.
  */
-public class LazyWriterResponse extends HttpServletResponseWrapper {
-
-    private final HttpServletRequest request;
-    private LazyWriter lazyWriter;
-    private PrintWriter writer;
+public class LazyWriterResponse extends com.psddev.dari.util.LazyWriterResponse {
 
     public LazyWriterResponse(HttpServletRequest request, HttpServletResponse response) {
-        super(response);
-        this.request = request;
-    }
-
-    public LazyWriter getLazyWriter() throws IOException {
-        if (lazyWriter == null) {
-            lazyWriter = new LazyWriter(request, super.getWriter());
-        }
-        return lazyWriter;
-    }
-
-    @Override
-    public PrintWriter getWriter() throws IOException {
-        if (writer == null) {
-            writer = new PrintWriter(getLazyWriter());
-        }
-        return writer;
+        super(request, response);
     }
 }
