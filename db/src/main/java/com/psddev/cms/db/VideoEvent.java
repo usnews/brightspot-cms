ec6a18b5bfbb5b00653660b7abbb1a2649e45753
ramanamadupuru <ramananaidu@gmail.com>
2013-12-19 14:28:09 -0500

    JSP which return progress bar information. VideoEvent to abstract events in a video

---
+++ db/src/main/java/com/psddev/cms/db/VideoEvent.java
@@ -0,0 +1,95 @@
+package com.psddev.cms.db;
+import java.util.HashMap;
+import java.util.Map;
+import com.psddev.dari.util.ObjectUtils;
+import com.psddev.dari.util.Settings;
+import com.psddev.dari.db.Record;
+import com.psddev.dari.db.*;
+import com.psddev.dari.util.*;
+@Record.Embedded
+/**
+** Abstracts an event in the video such as beginning of a chapter,ad etc
+**/
+public class VideoEvent extends Record {
+    public final static String VIDEO_EVENT_DESCRIPTION_METADATA_KEY="desc";
+    public final static String VIDEO_EVENT_THUMBNAIL_METADATA_KEY="thumbUrl";
+    @Indexed
+    private String name; 
+    private String  description;
+    public int startTime;
+    public int endTime;
+    private String tags;
+    private String  thumbnailUrl;
+    private String externalId;
+    private String entryId;
+   
+    public String getExternalId() {
+        return externalId;
+    }
+    public void setExternalId(String externalId) {
+        this.externalId = externalId;
+    }
+    public String getEntryId() {
+        return entryId;
+    }
+    public void setEntryId(String entryId) {
+        this.entryId = entryId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public int getStartTime() {
+        return startTime;
+    }
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+    public int getEndTime() {
+        return endTime;
+    }
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+    public String getTags() {
+        return tags;
+    }
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+    @Override
+    public  void beforeDelete() {
+        System.err.println("Control in video event delete ....");
+        if (externalId != null) {
+            System.err.println("Control in video event delete to delete...." + externalId);
+            String defaultVideoStorage=ObjectUtils.to(String.class,Settings.get("dari/defaultVideoStorage"));
+            TranscodingService ts=TranscodingServiceFactory.getTranscodingService(defaultVideoStorage);
+            ts.deleteEvent(externalId);
+        }
+    }
+    public VideoEvent() {
+    }
+    
+    public String toString() {
+        StringBuffer sb=new StringBuffer(" externalId:"+ externalId );
+        sb.append(", name:").append(name);
+        sb.append(", startTime:").append(startTime);
+        sb.append(", endTime:").append(endTime);
+        sb.append(", tags:").append(tags);
+        return sb.toString();
+    }
+}
---
+++ tool-ui/src/main/webapp/content/progress.jsp
@@ -0,0 +1,31 @@
+<%@ page import="java.util.List" %>
+<%@ page import="java.io.PrintWriter" %>
+<%@ page import="javax.servlet.http.HttpSession" %>
+<%@ page import="com.psddev.dari.util.UploadProgressListener" %>
+<%@ page import="com.psddev.dari.util.MultipartRequestFilter" %>
+<%@ page import="com.psddev.dari.util.MultipartRequest" %>
+<%@ page import="com.psddev.dari.util.JsonProcessor" %>
+<%@ page import="com.psddev.dari.util.UploadInfo" %>
+<%
+//response.setContentType("text/html");
+UploadInfo ui= new UploadInfo();
+JsonProcessor jsonProcessor=new JsonProcessor();
+//PrintWriter out = response.getWriter();
+//HttpSession session = request.getSession(true);
+if (session == null) {
+    ui.setMessage("Sorry, session is null");
+    out.println(jsonProcessor.generate(ui));
+    return;
+    //out.println("Sorry, session is null"); // just to be safe
+}
+
+UploadProgressListener listener= MultipartRequest.Static.getListenerFromSession(request);
+if (listener == null) {
+    ui.setMessage("Progress listener is null");
+    out.println(jsonProcessor.generate(ui));
+    return;
+}
+UploadInfo ui2= new UploadInfo(listener);
+out.println(jsonProcessor.generate(ui));
+%>
+
