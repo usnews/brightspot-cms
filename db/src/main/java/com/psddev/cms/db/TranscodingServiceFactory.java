516fef42fb1cd7b9ba802025d3d3bf2a418d29e5
ramanamadupuru <ramananaidu@gmail.com>
2013-12-19 10:17:59 -0500

    Added getDefault method to return default transcoding provider`

--- db/src/main/java/com/psddev/cms/db/TranscodingServiceFactory.java
+++ db/src/main/java/com/psddev/cms/db/TranscodingServiceFactory.java
@@ -1,6 +1,13 @@
 package com.psddev.cms.db; 
 import java.lang.IllegalArgumentException; 
+import java.com.psddev.dari.util.VideoStorageItem;
+
 public class TranscodingServiceFactory {
+         public static TranscodingService getDefault() {
+             if (VideoStorageItem.isDefaultStorageKaltura()) {
+                 return KalturaTranscodingService();
+             }
+         }
          public static TranscodingService getTranscodingService(String transcodingProvider) {
              if (transcodingProvider.equals("kaltura"))
              return new KalturaTranscodingService();
