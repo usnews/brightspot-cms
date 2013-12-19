ed197562a32f66cbb8a5bbc5d914ee2b0c064871
ramanamadupuru <ramananaidu@gmail.com>
2013-12-19 14:05:08 -0500

    Added methods related to events

--- db/src/main/java/com/psddev/cms/db/TranscodingService.java
+++ db/src/main/java/com/psddev/cms/db/TranscodingService.java
@@ -1,5 +1,8 @@
 package com.psddev.cms.db; 
 public interface TranscodingService {
-         public boolean updateTranscodingFlavorsInCms();
+     public boolean updateTranscodingFlavorsInCms();
+     public boolean updateThumbnailsInCms();
+     public void updateEventData(VideoContainer.Data videoData);
+     public void deleteEvent(String externalId);
 }
 
