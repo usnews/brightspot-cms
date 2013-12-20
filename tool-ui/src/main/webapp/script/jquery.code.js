864d4c3ab9ee77e5786bd31d684ca60465d8e58f
Hyoo Lim <hyoolim@gmail.com>
2013-12-20 13:42:57 -0500

    Fixes code editor scrolling the .inputContainer in a weird way on paste.

--- tool-ui/src/main/webapp/script/jquery.code.js
+++ tool-ui/src/main/webapp/script/jquery.code.js
@@ -7,13 +7,20 @@ var $window = $(window),
 
 $.plugin2('code', {
     '_create': function(input) {
-        var $input = $(input);
+        var $input = $(input),
+                mirror;
 
-        mirrors.push(CodeMirror.fromTextArea(input, {
+        mirror = CodeMirror.fromTextArea(input, {
             'indentUnit': 4,
             'lineNumbers': true,
             'mode': $input.attr('data-code-type')
-        }));
+        });
+
+        mirror.on('change', function() {
+            $input.closest('.inputContainer').scrollTop(0);
+        });
+
+        mirrors.push(mirror);
     }
 });
 
