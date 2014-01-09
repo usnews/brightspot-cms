7f10baf7d2e34b83cf68d57df0a43262dbe836aa
ramana.madupuru <rmadupuru@perfectsensedigital.com>
2014-01-09 16:28:47 -0600

    Added getVideos method to the interface and removed the TypeModification

--- db/src/main/java/com/psddev/cms/db/PlayListContainer.java
+++ db/src/main/java/com/psddev/cms/db/PlayListContainer.java
@@ -3,24 +3,21 @@ package com.psddev.cms.db;
 import java.util.Date;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.psddev.dari.db.Modification;
-import com.psddev.dari.db.ObjectField;
-import com.psddev.dari.db.ObjectType;
 import com.psddev.dari.db.Recordable;
 
 /**
  * Interface used to create a playlist
  */
 public interface PlayListContainer extends Recordable {
+
+    public List<VideoContainer> getVideos();
+
     /**
      * Modification which add information specific to playlist
      */
     @Modification.FieldInternalNamePrefix("cms.playlist.")
     public static final class Data extends Modification<PlayListContainer> {
-        private static final Logger LOGGER = LoggerFactory.getLogger(Data.class);
         @Indexed
         @ToolUi.ReadOnly
         private String externalId;
@@ -33,18 +30,11 @@ public interface PlayListContainer extends Recordable {
             this.externalId = externalId;
         }
 
-        public ObjectField getVideoContainerListItemField() {
-            return getState().getType().as(TypeModification.class).getVideoContainerListItemField();
-        }
-
-        public List<VideoContainer> getVideoContainers() {
-            return getVideoContainerListItemField() == null ? null : (List<VideoContainer>) getState().getByPath(getVideoContainerListItemField().getInternalName());
-        }
-
         @Override
         public void beforeSave() {
             VideoTranscodingService pls = VideoTranscodingServiceFactory.getDefault();
-            List<VideoContainer> videoContainers = getVideoContainers();
+            PlayListContainer PlayListContainer = (PlayListContainer) getState().getOriginalObject();
+            List<VideoContainer> videoContainers = PlayListContainer.getVideos();
             if (externalId == null) {
                 if (videoContainers != null & videoContainers.size() > 0) {
                     externalId = pls.createPlayList(Long.toString(new Date().getTime()), videoContainers);
@@ -60,26 +50,6 @@ public interface PlayListContainer extends Recordable {
                 VideoTranscodingService pls = VideoTranscodingServiceFactory.getDefault();
                 pls.deletePlayList(externalId);
             }
-        }
-    }
-
-    public static final class TypeModification extends Modification<ObjectType> {
-        // private static final Logger LOGGER =
-        // LoggerFactory.getLogger(TypeModification.class);
-
-        private ObjectField videoStorageItemField;
-
-        public ObjectField getVideoContainerListItemField() {
-            videoStorageItemField = null;
-            if (videoStorageItemField == null) {
-                for (ObjectField field : getOriginalObject().getFields()) {
-                    if (List.class.isAssignableFrom(field.getJavaField(getOriginalObject().getObjectClass()).getType())) {
-                        videoStorageItemField = field;
-                        break;
-                    }
-                }
-            }
-            return videoStorageItemField;
         }
     }
 
