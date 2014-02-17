0b71fef1008addd14b7524640122a4ba1b3b8d17
ramana.madupuru <rmadupuru@perfectsensedigital.com>
2014-02-17 15:17:57 -0600

    Updated logic to use bootstrap upload progress bar

--- tool-ui/src/main/webapp/script/progressbar.js
+++ tool-ui/src/main/webapp/script/progressbar.js
@@ -1,30 +1,25 @@
-function startProgress()
-{
-              document.getElementById('uploadStatus').style.display = 'inline';
-              document.getElementById('uploadProgressMessage').innerHTML = 'Upload Progress:';
-              document.getElementById('uploadProgressMessage').style.display = 'inline';
-              //document.getElementById('progressBarText').innerHTML = "0" + '%';
-              window.setTimeout("refreshProgress()", 1000);
-              return true;
+function startProgress() {
+    document.getElementById('cms-upload-progress').style.display = 'inline';
+    window.setTimeout("refreshProgress()", 50);
+    return true;
 }
 function refreshProgress() {
-              $.get('/cms/content/progress.jsp?' + new Date().getTime(),function(data,status){
-              var uploadInfo = $.parseJSON( data );
-              updateProgress(uploadInfo);
-              })
+    $.get('/cms/content/progress.jsp?t=' + new Date().getTime(), function (data, status) {
+        var uploadInfo = $.parseJSON(data);
+        updateProgress(uploadInfo);
+    });
 }
+
 function updateProgress(uploadInfo) {
-              // make sure you set the width style property for uploadProgressBar, otherwise progress.style.width won't work
-              var percentage=uploadInfo.percentDone;
-              var progress = document.getElementById("uploadProgressBar");
-              var indicator = document.getElementById("uploadIndicator");
-              var maxWidth = parseInt(progress.style.width) - 4;
-              var width = percentage * maxWidth / 100;
-              indicator.style.width = width + "px";
-              var perc = document.getElementById("uploadPercentage");
-              perc.innerHTML = percentage + "%";
-              if (percentage != 100)
-              window.setTimeout("refreshProgress()", 1000);
-              else 
-              document.getElementById('uploadStatus').style.display = "none";
+    var percentage = uploadInfo.percentDone;
+    var progress = document.getElementById("uploadprogress-bar");
+    progress.style.width = percentage + "%";
+    var percentProgress = document.getElementById("upload-percentage-status");
+    percentProgress.innerHTML=progress.style.width +" Complete";
+    if (percentage != 100) {
+        window.setTimeout("refreshProgress()", 500);
+    } else { //Delete progress data once it's finished
+       $.get('/cms/content/progress.jsp?action=delete&t=' + new Date().getTime(), function (data, status) {
+       });
+    }
 }
