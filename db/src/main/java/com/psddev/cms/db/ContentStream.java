b5f96b0fa458f5b8c572be4d4be839eaaa1cf958
Hyoo Lim <hyoolim@gmail.com>
2014-03-02 14:41:17 -0500

    Adds ContentStream.Mix to replace PinContentStream.

--- db/src/main/java/com/psddev/cms/db/ContentStream.java
+++ db/src/main/java/com/psddev/cms/db/ContentStream.java
@@ -1,18 +1,99 @@
 package com.psddev.cms.db;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.psddev.dari.db.Query;
 import com.psddev.dari.db.Record;
+import com.psddev.dari.util.ObjectUtils;
 
 @ContentStream.Embedded
 public abstract class ContentStream extends Record {
 
     public abstract List<?> findContents(int offset, int limit);
 
+    public static class Mix extends ContentStream {
+
+        private List<AreasStream> streams;
+
+        public List<AreasStream> getStreams() {
+            return streams;
+        }
+
+        public void setStreams(List<AreasStream> streams) {
+            this.streams = streams;
+        }
+
+        public List<?> findContents(int offset, int limit) {
+            List<Object> contents = new ArrayList<Object>();
+
+            for (int i = 0; i < limit; ++ i) {
+                contents.add(null);
+            }
+
+            for (AreasStream as : getStreams()) {
+                List<Integer> areas = new ArrayList<Integer>(as.getAreas());
+                int areasSize = areas.size();
+                List<?> streamContents = as.getStream().findContents(0, areas.size());
+                int streamContentsSize = streamContents.size();
+
+                Collections.sort(areas);
+
+                for (int i = 0; i < areasSize; ++ i) {
+                    contents.set(areas.get(i), i < streamContentsSize ? streamContents.get(i) : null);
+                }
+            }
+
+            return contents;
+        }
+    }
+
+    @Embedded
+    public static class AreasStream extends Record {
+
+        @Required
+        private Set<Integer> areas;
+
+        @Required
+        private ContentStream stream;
+
+        public Set<Integer> getAreas() {
+            if (areas == null) {
+                areas = new LinkedHashSet<Integer>();
+            }
+            return areas;
+        }
+
+        public void setAreas(Set<Integer> areas) {
+            this.areas = areas;
+        }
+
+        public ContentStream getStream() {
+            return stream;
+        }
+
+        public void setStream(ContentStream stream) {
+            this.stream = stream;
+        }
+
+        @Override
+        public String getLabel() {
+            StringBuilder label = new StringBuilder();
+            Set<Integer> areas = getAreas();
+
+            if (!ObjectUtils.isBlank(areas)) {
+                label.append(areas);
+            }
+
+            return label.toString();
+        }
+    }
+
     @SuppressWarnings("rawtypes")
-    public static class Search extends PinContentStream {
+    public static class Search extends ContentStream {
 
         @Embedded
         @Required
@@ -27,7 +108,7 @@ public abstract class ContentStream extends Record {
         }
 
         @Override
-        protected List<?> doFindContents(int offset, int limit) {
+        public List<?> findContents(int offset, int limit) {
             return getQuery().select(offset, limit).getItems();
         }
     }
