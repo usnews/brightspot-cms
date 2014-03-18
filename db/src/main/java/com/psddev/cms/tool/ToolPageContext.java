a32b589b1bf0d37469ca898577893f650fc6cbdd
Hyoo Lim <hyoolim@gmail.com>
2014-03-18 09:17:57 -0400

    Fixes the schedule date not setting the fake publish date when scheduling a new content immediately.

--- db/src/main/java/com/psddev/cms/tool/ToolPageContext.java
+++ db/src/main/java/com/psddev/cms/tool/ToolPageContext.java
@@ -2355,6 +2355,14 @@ public class ToolPageContext extends WebPageContext {
 
         State state = State.getInstance(object);
         Content.ObjectModification contentData = state.as(Content.ObjectModification.class);
+
+        if (state.isNew() ||
+                object instanceof Draft ||
+                contentData.isDraft() ||
+                state.as(Workflow.Data.class).getCurrentState() != null) {
+            setContentFormScheduleDate(object);
+        }
+
         Draft draft = getOverlaidDraft(object);
         UUID variationId = param(UUID.class, "variationId");
         Site site = getSite();
