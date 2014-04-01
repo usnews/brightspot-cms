342de8613852fb57d33d0bc064a109c1421af31c
Ryan Anderson <andersra88@gmail.com>
2014-04-01 14:46:15 -0400

    Updating Taxon for TaxonSearchResultRenderer Functionality
    - Adding indexed root field
    - Removing PullThrougCache
    - Adding Static method to get root Taxons

--- db/src/main/java/com/psddev/cms/db/Taxon.java
+++ db/src/main/java/com/psddev/cms/db/Taxon.java
@@ -1,62 +1,47 @@
 package com.psddev.cms.db;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
 
-import com.psddev.dari.db.Database;
+import com.psddev.cms.tool.TaxonSearchResultRenderer;
+import com.psddev.dari.db.Modification;
 import com.psddev.dari.db.Query;
 import com.psddev.dari.db.Recordable;
-import com.psddev.dari.util.PeriodicValue;
-import com.psddev.dari.util.PullThroughCache;
+import com.psddev.dari.util.PaginatedResult;
 
+@ToolUi.SearchResultRenderer(TaxonSearchResultRenderer.class)
 public interface Taxon extends Recordable {
 
     public boolean isRoot();
 
     public Collection<? extends Taxon> getChildren();
 
+    public static final class TaxonModification extends Modification<Taxon> {
+
+        private TaxonModification(){
+        }
+
+        @Indexed
+        @ToolUi.Hidden
+        private Boolean root;
+
+        public Boolean isRoot() {
+            return Boolean.TRUE.equals(root);
+        }
+
+        public void setRoot(Boolean root) {
+            this.root = root ? Boolean.TRUE : null;
+        }
+
+        public void beforeSave(){
+            this.setRoot(this.getOriginalObject().isRoot());
+        }
+    }
+
     /** {@link Taxon} utility methods. */
     public static final class Static {
 
-        private static final PullThroughCache<Class<? extends Taxon>, PeriodicValue<List<? extends Taxon>>>
-                TAXA_BY_CLASS = new PullThroughCache<Class<? extends Taxon>, PeriodicValue<List<? extends Taxon>>>() {
-
-            @Override
-            protected PeriodicValue<List<? extends Taxon>> produce(final Class<? extends Taxon> taxonClass) {
-                return new PeriodicValue<List<? extends Taxon>>() {
-
-                    @Override
-                    protected List<? extends Taxon> update() {
-                        Query<? extends Taxon> query = Query.from(taxonClass).using(Database.Static.getDefaultOriginal());
-                        Date cacheUpdate = getUpdateDate();
-                        Date databaseUpdate = query.lastUpdate();
-
-                        if (databaseUpdate == null || (cacheUpdate != null && !databaseUpdate.after(cacheUpdate))) {
-                            List<? extends Taxon> taxa = get();
-
-                            return taxa != null ? taxa : Collections.<Taxon>emptyList();
-                        }
-
-                        return query.selectAll();
-                    }
-                };
-            }
-        };
-
-        @SuppressWarnings("unchecked")
-        public static <T extends Taxon> List<T> getRoots(Class<T> taxonClass) {
-            List<T> roots = new ArrayList<T>();
-            
-            for (Taxon t : TAXA_BY_CLASS.get(taxonClass).get()) {
-                if (t.isRoot()) {
-                    roots.add((T) t);
-                }
-            }
-
-            return roots;
+        public static <T extends Taxon> PaginatedResult<T> getRoots(Class<T> taxonClass, long offset, int limit) {
+            return Query.from(taxonClass).where("root = true").select(offset, limit);
         }
     }
 }
