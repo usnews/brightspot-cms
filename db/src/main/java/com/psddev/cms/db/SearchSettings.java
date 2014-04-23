d847be3029220ec7f940ae820fd20625bcd59df5
kelly <kelly@perfectsensedigital.com>
2014-04-23 18:32:44 -0400

    Trying to find the right purpose for the search settings object

--- db/src/main/java/com/psddev/cms/db/SearchSettings.java
+++ db/src/main/java/com/psddev/cms/db/SearchSettings.java
@@ -1,39 +1,63 @@
 package com.psddev.cms.db;
 
-import com.psddev.dari.db.ObjectType;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class SearchSettings extends Content {
 
-    //TODO: I'm not sure that this is correct to put in the search settings since each type will require a renderer
-    @CollectionMinimum(1)
-    private Set<ObjectType> types;
-    @Embedded
-    //TODO: maybe these embedded objects are wrapped so that the rules can be used in more than one search
     private List<SearchQueryBuilder.Rule> rules = new ArrayList<SearchQueryBuilder.Rule>();
-
-    public Set<ObjectType> getTypes() {
-        if (types == null) {
-            setTypes(new HashSet<ObjectType>());
-        }
-        return types;
-    }
-
-    public void setTypes(Set<ObjectType> types) {
-        this.types = types;
-    }
+    /*@Embedded
+    private List<QueryRule> rules = new ArrayList<QueryRule>();
+    */
 
     public List<SearchQueryBuilder.Rule> getRules() {
-        if (rules == null) {
-            setRules(new ArrayList<SearchQueryBuilder.Rule>());
-        }
         return rules;
     }
 
     public void setRules(List<SearchQueryBuilder.Rule> rules) {
         this.rules = rules;
     }
+
+    /*public List<SearchQueryBuilder.Rule> getRules() {
+        List<SearchQueryBuilder.Rule> ruleList = new ArrayList<SearchQueryBuilder.Rule>();
+        for(QueryRule queryRule:this.rules){
+            ruleList.add(queryRule.getRule());
+        }
+        return ruleList;
+    }
+
+    public void setRules(List<SearchQueryBuilder.Rule> rules) {
+        if(this.rules == null) {
+            this.rules = new ArrayList<QueryRule>();
+        }
+        for(SearchQueryBuilder.Rule rule:rules){
+            this.rules.add(new QueryRule(rule));
+        }
+    }
+
+    public static class QueryRule extends Record {
+
+        SearchQueryBuilder.Rule rule;
+
+        public QueryRule(){}
+
+        public QueryRule(SearchQueryBuilder.Rule rule){
+            this.rule = rule;
+        }
+
+        public String getLabel(){
+            if(!ObjectUtils.isBlank(rule)) {
+                return rule.getClass().getSimpleName();
+            }
+            return "";
+        }
+
+        public SearchQueryBuilder.Rule getRule() {
+            return rule;
+        }
+
+        public void setRule(SearchQueryBuilder.Rule rule) {
+            this.rule = rule;
+        }
+    }*/
 }
