40376eb8922499d40d9a41f155e4e6e5e61dd3d0
Hyoo Lim <hyoolim@gmail.com>
2014-05-05 14:44:05 -0400

    Moves content field diff JS logic to content/diff module.

--- tool-ui/src/main/webapp/script/cms.js
+++ tool-ui/src/main/webapp/script/cms.js
@@ -63,8 +63,9 @@ require([
     'jquery.tabbed',
     'jquery.toggleable',
     'jquery.widthaware',
-    'diff',
-    'nv.d3' ],
+    'nv.d3',
+
+    'content' ],
 
 function() {
     var $ = arguments[0];
@@ -365,135 +366,6 @@ function() {
         var selectedClass = 'state-selected';
         $this.closest('ul').find('.' + selectedClass).removeClass(selectedClass);
         $this.closest('li').addClass(selectedClass);
-    });
-
-    // Content diff with a side by side view.
-    $doc.onCreate('.contentDiff', function() {
-        var $container = $(this),
-                $tabs,
-                $tabEdit,
-                $tabSideBySide,
-                $left = $container.find('> .contentDiffLeft'),
-                $right = $container.find('> .contentDiffRight'),
-                getValues;
-
-        $tabs = $('<ul/>', {
-            'class': 'tabs'
-        });
-
-        $tabEdit = $('<li/>', {
-            'html': $('<a/>', {
-                'text': 'Edit',
-                'click': function() {
-                    $container.trigger('contentDiff-edit');
-                    return false;
-                }
-            })
-        });
-
-        $tabSideBySide = $('<li/>', {
-            'html': $('<a/>', {
-                'text': 'Side By Side',
-                'click': function() {
-                    $container.trigger('contentDiff-sideBySide');
-                    return false;
-                }
-            })
-        });
-
-        $container.bind('contentDiff-edit', function() {
-            $container.add($('.widget-publishing')).removeClass('contentDiff-sideBySide').addClass('contentDiff-edit');
-            $tabs.find('li').removeClass('state-selected');
-            $tabEdit.addClass('state-selected');
-
-            $left.find('> .objectInputs > .tabs').css('height', '');
-            $left.find('> .objectInputs > .inputContainer').css('height', '');
-            $right.find('> .objectInputs > .tabs').css('height', '');
-            $right.find('> .objectInputs > .inputContainer').css('height', '');
-        });
-
-        $container.bind('contentDiff-sideBySide', function() {
-            $container.add($('.widget-publishing')).removeClass('contentDiff-edit').addClass('contentDiff-sideBySide');
-            $tabs.find('li').removeClass('state-selected');
-            $tabSideBySide.addClass('state-selected');
-
-            function equalizeHeight($left, $right) {
-                setTimeout(function() {
-                    $left.add($right).height(Math.max($left.height(), $right.height()));
-                }, 100);
-            }
-
-            $left.add($right).find('.collapsed').removeClass('collapsed');
-
-            $left.find('> .objectInputs > .tabs').each(function() {
-                var $leftTabs = $(this);
-
-                equalizeHeight($leftTabs, $right.find('> .objectInputs > .tabs'));
-            });
-
-            $left.find('> .objectInputs > .inputContainer').each(function() {
-                var $leftInput = $(this);
-
-                equalizeHeight($leftInput, $right.find('> .objectInputs > .inputContainer[data-field="' + $leftInput.attr('data-field') + '"]'));
-            });
-        });
-
-        getValues = function($input) {
-            return $input.
-                    find(':input, select, textarea').
-                    serialize().
-                    replace(new RegExp('(^|&)[^%]+%2F', 'g'), '$1%2F');
-        };
-
-        $left.find('> .objectInputs > .inputContainer').each(function() {
-            var $leftInput = $(this),
-                    $rightInput = $right.find('> .objectInputs > .inputContainer[data-field="' + $leftInput.attr('data-field') + '"]'),
-                    height = Math.max($leftInput.outerHeight(true), $rightInput.outerHeight(true));
-
-            if (getValues($leftInput) === getValues($rightInput)) {
-                $leftInput.addClass('contentDiffSame');
-                $rightInput.addClass('contentDiffSame');
-            }
-        });
-
-        $left.find('> .objectInputs > .inputContainer > .inputSmall > textarea:not(.richtext)').each(function() {
-            var $leftText = $(this),
-                    $rightText = $right.find('> .objectInputs > .inputContainer[data-field="' + $leftText.closest('.inputContainer').attr('data-field') + '"] textarea:not(.richtext)'),
-                    left = $leftText.val(),
-                    right = $rightText.val(),
-                    diffs = JsDiff.diffWords(left, right),
-                    $leftCopy = $('<div/>', { 'class': 'contentDiffCopy' }),
-                    $rightCopy = $('<div/>', { 'class': 'contentDiffCopy' });
-
-            $.each(diffs, function(i, diff) {
-                if (!diff.added) {
-                    $leftCopy.append(diff.removed ?
-                            $('<span/>', { 'class': 'contentDiffRemoved', 'text': diff.value }) :
-                            diff.value);
-                }
-            });
-
-            $.each(diffs, function(i, diff) {
-                if (!diff.removed) {
-                    $rightCopy.append(diff.added ?
-                            $('<span/>', { 'class': 'contentDiffAdded', 'text': diff.value }) :
-                            diff.value);
-                }
-            });
-
-            $leftText.addClass('contentDiffText');
-            $leftText.before($leftCopy);
-
-            $rightText.addClass('contentDiffText');
-            $rightText.before($rightCopy);
-        });
-
-        $tabs.append($tabEdit);
-        $tabs.append($tabSideBySide);
-        $container.prepend($tabs);
-        $container.trigger($right.is('.contentDiffCurrent') ?
-                'contentDiff-sideBySide' :
-                'contentDiff-edit');
     });
 
     $doc.onCreate('.searchAdvancedResult', function() {
---
+++ tool-ui/src/main/webapp/script/content.js
@@ -0,0 +1,5 @@
+define([
+    'content/diff' ],
+
+function() {
+});
