0316eee7ec0ea77dd0f33a925be6a883ca6f01c1
kelly <kelly@perfectsensedigital.com>
2014-06-01 10:28:00 -0400

    Enhance the EditorialSearch experience

--- db/src/main/java/com/psddev/cms/db/EditorialSearchSettings.java
+++ db/src/main/java/com/psddev/cms/db/EditorialSearchSettings.java
@@ -1,9 +1,13 @@
 package com.psddev.cms.db;
 
+import com.psddev.dari.db.ObjectType;
+import com.psddev.dari.db.Query;
 import com.psddev.dari.util.ObjectUtils;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class EditorialSearchSettings extends Content {
 
@@ -11,7 +15,12 @@ public class EditorialSearchSettings extends Content {
     private SearchQueryBuilder.StopWords stopWords;
     @Embedded
     private SearchQueryBuilder.Synonyms synonyms;
-    //TODO: how do we want to surface rules?
+
+    private transient String searchString;
+
+    public EditorialSearchSettings() {
+        setStopWords(new SearchQueryBuilder.StopWords());
+    }
 
     public SearchQueryBuilder.StopWords getStopWords() {
         return stopWords;
@@ -29,8 +38,28 @@ public class EditorialSearchSettings extends Content {
         this.synonyms = synonyms;
     }
 
-    public List<SearchQueryBuilder.Rule> toQuery() {
-        return null;
+    public String getSearchString() {
+        return searchString;
+    }
+
+    public void setSearchString(String searchString) {
+        this.searchString = searchString;
+    }
+
+    public Query<?> getQuery() {
+        //initialize the query for search
+        SearchQueryBuilder searchQuery = new SearchQueryBuilder();
+
+        //set the types that you will search against
+        Set<ObjectType> types = new HashSet<ObjectType>();
+        types.addAll(ObjectType.getInstance(Content.class).findConcreteTypes());
+        searchQuery.setTypes(types);
+
+        //set the initial rules, followed by the ones from the cms
+        searchQuery.addRules(this.getEditorialRules());
+
+        //return the query
+        return searchQuery.toQuery(getSearchString());
     }
 
     public List<SearchQueryBuilder.Rule> getEditorialRules() {
