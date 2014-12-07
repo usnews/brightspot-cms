7347bc63bca83df31bbb5245f2a3c30b92a29a26
Bryan Morgan <bryanmorgan@gmail.com>
2014-12-07 10:22:09 -0800

    Upgrade Leaflet and fix issues with Location search and marker placement
    * Upgrade Leaflet to 0.7.3
    * Add a current location button using the leaflet-locatecontrol plugin to update the marker location
    * Fix #42 - prevent enter from submitting CMS form and move marker when location is found

--- tool-ui/src/main/webapp/script/input/location.js
+++ tool-ui/src/main/webapp/script/input/location.js
@@ -20,14 +20,12 @@ function($) {
             var lat = $(latInput).val();
             var lng = $(longInput).val();
             var zoom = $(zoomInput).val();
-            var showMarker = true;
 
             if ((!lat || lat === '' || lat.length === 0) ||
                 (!lng || lng === '' || lng.length === 0)) {
                 lat = 39.8282;
                 lng = -98.5795;
                 zoom = 4;
-                showMarker = false;
             }
 
             map.setView([lat, lng], zoom);
@@ -36,32 +34,60 @@ function($) {
                 zoomInput.val(map.getZoom());
             });
 
-            var marker = null;
-            if (showMarker) {
-                marker = L.marker([lat, lng], { draggable: true }).addTo(map);
-                marker.on('dragend', function(e) {
-                    plugin.updateLatLng(e.target, map, latInput, longInput, zoomInput);
-                });
-            }
+            // Add a location marker to the map
+            var marker = L.marker([lat, lng], { draggable: true}).addTo(map);
+            marker.on('dragend', function(e) {
+                plugin.updateLatLng(e.target, map, latInput, longInput, zoomInput);
+            });
+
+            // Update the marker's location with a new lat/long value
+            var updateMarkerLocation = function(latitude, longitude) {
+                if (!map.hasLayer(marker)) {
+                    marker.addTo(map);
+                }
+                marker.setLatLng([latitude, longitude]);
+                plugin.updateLatLng(marker, map, latInput, longInput, zoomInput);
+            };
+
+            // --- Geocoder Address Search
 
             new L.Control.GeoSearch({
                 zoomLevel: 16,
-                provider: new L.GeoSearch.Provider.OpenStreetMap(),
-                success: function(m) {
-                    if (marker !== null && m !== marker) {
-                        map.removeLayer(marker);
-                    }
+                showMarker: false,
+                provider: new L.GeoSearch.Provider.OpenStreetMap()
+            }).addTo(map);
 
-                    marker = m;
-                    marker.dragging.enable();
+            // Update marker location after a geo search result is found
+            map.on("geosearch_foundlocations", function(event) {
+                if (event.Locations && event.Locations.length > 0) {
+                    updateMarkerLocation(event.Locations[0].Y, event.Locations[0].X);
+                }
+            });
 
-                    marker.on('dragend', function(e) {
-                        plugin.updateLatLng(e.target, map, latInput, longInput, zoomInput);
-                    });
+            // Prevent the enter key in the address search from submitting the CMS page form
+            $("#leaflet-control-geosearch-qry").keypress(function(event) {
+                if (event.which === 13) {
+                    event.preventDefault();
+                }
+            });
 
-                    plugin.updateLatLng(marker, map, latInput, longInput, zoomInput);
+            // --- Current Location
+
+            // Add a button to mark to user's current location, using HTML5 geolocation
+            L.control.locate({
+                drawCircle: false,
+                icon: 'icon icon-map-marker',
+                iconLoading: 'icon icon-rss',
+                strings: {
+                    title: "Set marker to your current location",
+                    popup: "<b>Current location</b><br/>(within {distance} {unit})"
                 }
             }).addTo(map);
+
+            // Update the location marker when we find the user's current location
+            map.on("locationfound", function(event) {
+                updateMarkerLocation(event.latitude, event.longitude);
+            });
         },
 
         'updateLatLng' : function(marker, map, latInput, longInput, zoomInput) {
