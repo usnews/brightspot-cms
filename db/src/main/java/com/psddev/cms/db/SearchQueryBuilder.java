c9794601d3b6cd92bac44e0c7480e8b836bd53ef
kelly <kelly@perfectsensedigital.com>
2014-12-09 10:24:26 -0500

    Add multisite capability to SearchQueryBuilder.

--- db/src/main/java/com/psddev/cms/db/SearchQueryBuilder.java
+++ db/src/main/java/com/psddev/cms/db/SearchQueryBuilder.java
@@ -212,6 +212,32 @@ public class SearchQueryBuilder extends Record {
         return normalized;
     }
 
+    public Query toQuery(Site site, Object... terms) {
+        List<String> queryTerms = normalizeTerms(terms);
+        Query query = Query.from(Object.class);
+
+        if (site != null) {
+            query.and(site.itemsPredicate());
+        }
+
+        for (Rule rule : getRules()) {
+            rule.apply(this, query, queryTerms);
+        }
+
+        if (!queryTerms.isEmpty()) {
+            query.and("_any matchesAny ?", queryTerms);
+        }
+
+        Set<ObjectType> allTypes = new HashSet<ObjectType>();
+        for (ObjectType type : getTypes()) {
+            allTypes.addAll(type.as(ToolUi.class).findDisplayTypes());
+        }
+        query.and("_type = ?", allTypes);
+
+        return query;
+    }
+
+    @Deprecated
     public Query toQuery(Object... terms) {
         List<String> queryTerms = normalizeTerms(terms);
         Query query = Query.from(Object.class);
