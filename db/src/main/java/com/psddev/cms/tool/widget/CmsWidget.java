afcacd55eaed59647280771b9277aad57bbf72ca
Ryan-Anderson <ryan@perfectsensedigital.com>
2014-12-11 17:17:37 -0500

    Makes all CMS Widgets embedded

--- db/src/main/java/com/psddev/cms/tool/widget/CmsWidget.java
+++ db/src/main/java/com/psddev/cms/tool/widget/CmsWidget.java
@@ -3,6 +3,7 @@ package com.psddev.cms.tool.widget;
 import com.psddev.cms.tool.DashboardWidget;
 import com.psddev.dari.db.Recordable;
 
+@Recordable.Embedded
 public interface CmsWidget extends Recordable, DashboardWidget {
 
 }
