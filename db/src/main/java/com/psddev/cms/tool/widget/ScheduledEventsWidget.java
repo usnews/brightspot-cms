3d500756b84fd83ee15f6038976b3c706a5269c2
Ryan-Anderson <ryan@perfectsensedigital.com>
2014-12-15 12:08:12 -0500

    Adds daily/weekly/monthly view of schedule to ScheduledEventsWidget

--- db/src/main/java/com/psddev/cms/tool/widget/ScheduledEventsWidget.java
+++ db/src/main/java/com/psddev/cms/tool/widget/ScheduledEventsWidget.java
@@ -68,32 +68,7 @@ public class ScheduledEventsWidget extends DefaultDashboardWidget {
         page.writeStart("div", "class", "widget widget-scheduledEvents" + (hasSchedules ? "" : " widget-scheduledEvents-empty"));
             page.writeStart("h1", "class", "icon icon-action-schedule");
 
-                page.writeHtml("Scheduled Events: ");
-
-                String beginMonth = begin.monthOfYear().getAsText();
-                int beginYear = begin.year().get();
-                String endMonth = end.monthOfYear().getAsText();
-                int endYear = end.year().get();
-
-                page.writeHtml(beginMonth);
-                page.writeHtml(" ");
-                page.writeHtml(begin.dayOfMonth().get());
-
-                if (beginYear != endYear) {
-                    page.writeHtml(", ");
-                    page.writeHtml(beginYear);
-                }
-
-                page.writeHtml(" - ");
-
-                if (!endMonth.equals(beginMonth)) {
-                    page.writeHtml(endMonth);
-                    page.writeHtml(" ");
-                }
-
-                page.writeHtml(end.dayOfMonth().get());
-                page.writeHtml(", ");
-                page.writeHtml(endYear);
+                page.writeHtml("Scheduled Events");
 
             page.writeEnd();
 
@@ -117,19 +92,43 @@ public class ScheduledEventsWidget extends DefaultDashboardWidget {
                 page.writeEnd();
             page.writeEnd();
 
-            /*
-            page.writeStart("form", "method", "get", "data-bsp-autosubmit", "", "action", page.url(null));
-                page.writeStart("select", "name", "mode");
-                    for (Mode m : Mode.values()) {
-                        page.writeStart("option",
-                                "value", m.name(),
-                                "selected", m.equals(mode) ? "selected" : null);
-                            page.writeHtml(m);
+            page.writeStart("ul", "class", "button-group");
+                for (Mode m : Mode.values()) {
+                    page.writeStart("li", "class", (m.equals(mode) ? "selected" : ""));
+                        page.writeStart("a",
+                                "href", page.url("", "mode", m.name()));
+                            page.writeHtml(m.displayName);
                         page.writeEnd();
-                    }
-                page.writeEnd();
+                    page.writeEnd();
+                }
             page.writeEnd();
-            */
+
+            String beginMonth = begin.monthOfYear().getAsText();
+            int beginYear = begin.year().get();
+            String endMonth = end.monthOfYear().getAsText();
+            int endYear = end.year().get();
+
+            page.writeStart("div");
+                page.writeHtml(beginMonth);
+                page.writeHtml(" ");
+                page.writeHtml(begin.dayOfMonth().get());
+
+                if (beginYear != endYear) {
+                    page.writeHtml(", ");
+                    page.writeHtml(beginYear);
+                }
+
+                page.writeHtml(" - ");
+
+                if (!endMonth.equals(beginMonth)) {
+                    page.writeHtml(endMonth);
+                    page.writeHtml(" ");
+                }
+
+                page.writeHtml(end.dayOfMonth().get());
+                page.writeHtml(", ");
+                page.writeHtml(endYear);
+            page.writeEnd();
 
             page.writeStart("ul", "class", "pagination");
 
@@ -145,12 +144,12 @@ public class ScheduledEventsWidget extends DefaultDashboardWidget {
                     page.writeEnd();
                 }
 
-                page.writeStart("li");
-                    page.writeStart("a",
-                            "href", page.url("", "date", System.currentTimeMillis()));
-                        page.writeHtml("Today");
-                    page.writeEnd();
-                page.writeEnd();
+//                page.writeStart("li");
+//                    page.writeStart("a",
+//                            "href", page.url("", "date", System.currentTimeMillis()));
+//                        page.writeHtml("Today");
+//                    page.writeEnd();
+//                page.writeEnd();
 
                 page.writeStart("li", "class", "next");
                     page.writeStart("a",
@@ -166,6 +165,33 @@ public class ScheduledEventsWidget extends DefaultDashboardWidget {
     }
 
     private enum Mode {
+        DAY("Day") {
+
+            @Override
+            public DateTime getBegin(DateTime date) {
+                return date.toDateMidnight().toDateTime();
+            }
+
+            @Override
+            public DateTime getEnd(DateTime date) {
+                return getBegin(date).plusDays(1);
+            }
+
+            @Override
+            public DateTime getPrevious(DateTime date) {
+                return date.plusDays(-1);
+            }
+
+            @Override
+            public DateTime getNext(DateTime date) {
+                return date.plusDays(1);
+            }
+
+            @Override
+            public void display(ToolPageContext page, Map<DateTime, List<Schedule>> schedulesByDate) throws IOException {
+                displayAgendaView(page, schedulesByDate);
+            }
+        },
         WEEK("Week") {
 
             @Override
@@ -190,62 +216,7 @@ public class ScheduledEventsWidget extends DefaultDashboardWidget {
 
             @Override
             public void display(ToolPageContext page, Map<DateTime, List<Schedule>> schedulesByDate) throws IOException {
-                page.writeStart("div", "class", "calendar calendar-week");
-                    for (Map.Entry<DateTime, List<Schedule>> entry : schedulesByDate.entrySet()) {
-                        DateTime date = entry.getKey();
-                        List<Schedule> schedules = entry.getValue();
-
-                        page.writeStart("div", "class", "calendarRow");
-                            page.writeStart("div", "class", "calendarDay" + (date.equals(new DateTime(null, page.getUserDateTimeZone()).toDateMidnight()) ? " calendarDay-today" : ""));
-                                page.writeStart("span", "class", "calendarDayOfWeek").writeHtml(date.dayOfWeek().getAsShortText()).writeEnd();
-                                page.writeStart("span", "class", "calendarDayOfMonth").writeHtml(date.dayOfMonth().get()).writeEnd();
-                            page.writeEnd();
-
-                            page.writeStart("div", "class", "calendarCell").writeStart("table", "class", "links table-striped pageThumbnails").writeStart("tbody");
-                                for (Schedule schedule : schedules) {
-                                    DateTime triggerDate = page.toUserDateTime(schedule.getTriggerDate());
-                                    List<Object> drafts = Query.fromAll().where("com.psddev.cms.db.Draft/schedule = ?", schedule).selectAll();
-
-                                    if (drafts.isEmpty()) {
-                                        continue;
-                                    }
-
-                                    boolean first = true;
-
-                                    for (Object d : drafts) {
-                                        if (!(d instanceof Draft)) {
-                                            continue;
-                                        }
-
-                                        Draft draft = (Draft) d;
-                                        Object draftObject = draft.getObject();
-
-                                        page.writeStart("tr", "data-preview-url", "/_preview?_cms.db.previewId=" + draft.getId());
-                                            page.writeStart("td", "class", "time");
-                                                if (first) {
-                                                    page.writeHtml(triggerDate.toString("hh:mm a"));
-                                                    first = false;
-                                                }
-                                            page.writeEnd();
-
-                                            page.writeStart("td");
-                                                page.writeTypeLabel(draftObject);
-                                            page.writeEnd();
-
-                                            page.writeStart("td", "data-preview-anchor", "");
-                                                page.writeStart("a",
-                                                        "href", page.objectUrl("/content/edit.jsp", draft),
-                                                        "target", "_top");
-                                                    page.writeObjectLabel(draftObject);
-                                                page.writeEnd();
-                                            page.writeEnd();
-                                        page.writeEnd();
-                                    }
-                                }
-                            page.writeEnd().writeEnd().writeEnd();
-                        page.writeEnd();
-                    }
-                page.writeEnd();
+                displayAgendaView(page, schedulesByDate);
             }
         },
 
@@ -273,6 +244,58 @@ public class ScheduledEventsWidget extends DefaultDashboardWidget {
 
             @Override
             public void display(ToolPageContext page, Map<DateTime, List<Schedule>> schedulesByDate) throws IOException {
+                page.writeStart("div", "class", "calendar calendar-month");
+
+                for (Map.Entry<DateTime, List<Schedule>> entry : schedulesByDate.entrySet()) {
+                    DateTime date = entry.getKey();
+                    List<Schedule> schedules = entry.getValue();
+
+                    if (date.getDayOfMonth() == 1 || date.getDayOfWeek() == 1) {
+                        page.writeStart("div", "class", "calendarRow");
+
+                        if(date.getDayOfMonth() == 1) {
+                            int offset = date.getDayOfWeek() - 1;
+                            for(int i=0; i < offset; i++) {
+                                page.writeStart("div", "class", "calendarDay", "style", "visibility:hidden;").writeEnd();
+                            }
+                        }
+                    }
+
+                    page.writeStart("div", "class", "calendarDay" + (date.equals(new DateTime(null, page.getUserDateTimeZone()).toDateMidnight()) ? " calendarDay-today" : "") + (" day-of-week-" + date.getDayOfWeek()));
+                        page.writeStart("span", "class", "calendarDayOfWeek").writeHtml(date.dayOfWeek().getAsShortText()).writeEnd();
+                        page.writeStart("span", "class", "calendarDayOfMonth").writeHtml(date.dayOfMonth().get()).writeEnd();
+
+                        for (Schedule schedule : schedules) {
+                            List<Object> drafts = Query.fromAll().where("com.psddev.cms.db.Draft/schedule = ?", schedule).selectAll();
+
+                            if (drafts.isEmpty()) {
+                                continue;
+                            }
+
+                            int draftCount = drafts.size();
+
+                            page.writeStart("div", "class", "calendarEventsContainer");
+                                page.writeStart("div", "class", "calendarEvents");
+
+                                    page.writeStart("div", "class", "count");
+                                        page.writeHtml(draftCount);
+                                    page.writeEnd();
+                                    page.writeStart("div", "class", "label");
+                                        page.writeHtml("Event" + (draftCount > 1 ? "s" : ""));
+                                    page.writeEnd();
+
+                                page.writeEnd();
+                            page.writeEnd();
+                        }
+
+                    page.writeEnd();
+
+                    if (date.getDayOfMonth() == 31 || date.getDayOfWeek() == 7) {
+                        page.writeEnd();
+                    }
+
+                }
+                page.writeEnd();
             }
         };
 
@@ -295,6 +318,65 @@ public class ScheduledEventsWidget extends DefaultDashboardWidget {
         @Override
         public String toString() {
             return displayName;
+        }
+
+        public static void displayAgendaView(ToolPageContext page, Map<DateTime, List<Schedule>> schedulesByDate) throws IOException{
+            page.writeStart("div", "class", "calendar calendar-week");
+            for (Map.Entry<DateTime, List<Schedule>> entry : schedulesByDate.entrySet()) {
+                DateTime date = entry.getKey();
+                List<Schedule> schedules = entry.getValue();
+
+                page.writeStart("div", "class", "calendarRow");
+                page.writeStart("div", "class", "calendarDay" + (date.equals(new DateTime(null, page.getUserDateTimeZone()).toDateMidnight()) ? " calendarDay-today" : ""));
+                page.writeStart("span", "class", "calendarDayOfWeek").writeHtml(date.dayOfWeek().getAsShortText()).writeEnd();
+                page.writeStart("span", "class", "calendarDayOfMonth").writeHtml(date.dayOfMonth().get()).writeEnd();
+                page.writeEnd();
+
+                page.writeStart("div", "class", "calendarCell").writeStart("table", "class", "links table-striped pageThumbnails").writeStart("tbody");
+                for (Schedule schedule : schedules) {
+                    DateTime triggerDate = page.toUserDateTime(schedule.getTriggerDate());
+                    List<Object> drafts = Query.fromAll().where("com.psddev.cms.db.Draft/schedule = ?", schedule).selectAll();
+
+                    if (drafts.isEmpty()) {
+                        continue;
+                    }
+
+                    boolean first = true;
+
+                    for (Object d : drafts) {
+                        if (!(d instanceof Draft)) {
+                            continue;
+                        }
+
+                        Draft draft = (Draft) d;
+                        Object draftObject = draft.getObject();
+
+                        page.writeStart("tr", "data-preview-url", "/_preview?_cms.db.previewId=" + draft.getId());
+                        page.writeStart("td", "class", "time");
+                        if (first) {
+                            page.writeHtml(triggerDate.toString("hh:mm a"));
+                            first = false;
+                        }
+                        page.writeEnd();
+
+                        page.writeStart("td");
+                        page.writeTypeLabel(draftObject);
+                        page.writeEnd();
+
+                        page.writeStart("td", "data-preview-anchor", "");
+                        page.writeStart("a",
+                                "href", page.objectUrl("/content/edit.jsp", draft),
+                                "target", "_top");
+                        page.writeObjectLabel(draftObject);
+                        page.writeEnd();
+                        page.writeEnd();
+                        page.writeEnd();
+                    }
+                }
+                page.writeEnd().writeEnd().writeEnd();
+                page.writeEnd();
+            }
+            page.writeEnd();
         }
     }
 }
