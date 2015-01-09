463a515f8e6b7966106d4856024ce1fce523e659
Patrick Fitzgerald <pat@perfectsensedigital.com>
2015-01-09 15:06:45 -0500

    Carousel search results show and hide carousel nav
    Add code to detect if we have more search results to fetch, then hide the next/previous buttons when at the start or end of the carousel if there are no more results to fetch.

--- tool-ui/src/main/webapp/script/v3/searchcarousel.js
+++ tool-ui/src/main/webapp/script/v3/searchcarousel.js
@@ -33,12 +33,17 @@ define([ 'jquery', 'bsp-utils', 'v3/input/carousel' ], function($, bsp_utils, ca
           'url'    : $container.data(dataAttr),
           'cache'  : false
         }).done(function(html) {
-          var $searchCarousel = $(html).find(settings.containerSelector).first();
+            
+            var $searchCarousel = $(html).find(settings.containerSelector).first();
+            var url = $searchCarousel.data(dataAttr);
 
-          // Update the data attribute with the URL to fetch the next set of results
-          $container.data(dataAttr, $searchCarousel.data(dataAttr));
-          addTiles($searchCarousel.children(settings.itemsSelector), !isEndEvent);
-          data.carousel.update();
+            // Update the data attribute with the URL to fetch the next set of results
+            // Note we use jQuery.data() to set the data,
+            // but we also change the data- attribute on the elment so it can be used by CSS.
+            $container.data(dataAttr, url).attr('data-' + dataAttr, url);
+
+            addTiles($searchCarousel.children(settings.itemsSelector), !isEndEvent);
+            data.carousel.update();
         });
 
       });
