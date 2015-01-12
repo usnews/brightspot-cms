61adbdf3ffd5ef7e9c143bc14c5e3e302fe6187f
Patrick Fitzgerald <pat@perfectsensedigital.com>
2015-01-12 14:05:25 -0500

    Search carousel change to 4 space indent
    Changing indentation to match other code.

--- tool-ui/src/main/webapp/script/v3/searchcarousel.js
+++ tool-ui/src/main/webapp/script/v3/searchcarousel.js
@@ -1,93 +1,93 @@
 define([ 'jquery', 'bsp-utils', 'v3/input/carousel' ], function($, bsp_utils, carouselUtility) {
 
-  var settings  = {
-    containerSelector         : '.searchResult-carousel',
-    nextAttr                  : 'next-page',
-    prevAttr                  : 'prev-page',
-    itemsSelector             : 'a'
-  };
+    var settings  = {
+        containerSelector         : '.searchResult-carousel',
+        nextAttr                  : 'next-page',
+        prevAttr                  : 'prev-page',
+        itemsSelector             : 'a'
+    };
 
-  bsp_utils.onDomInsert(document, settings.containerSelector, {
+    bsp_utils.onDomInsert(document, settings.containerSelector, {
 
-    insert : function (container) {
+        insert : function (container) {
 
-      var carousel;
+            var carousel;
 
-      var $container = $(container);
-      carousel = Object.create(carouselUtility);
-      carousel.init($container, {numbered:false});
+            var $container = $(container);
+            carousel = Object.create(carouselUtility);
+            carousel.init($container, {numbered:false});
 
-      addTiles($container.children(settings.itemsSelector), false);
-      carousel.update();
+            addTiles($container.children(settings.itemsSelector), false);
+            carousel.update();
 
-      // In case the active tile is not within the window we should center it
-      // (note this might trigger even more tiles to load)
-      carousel.goToActiveTile();
-        
-      // Add more tiles via ajax when carousel ends
-      $container.on('carousel.end carousel.begin', function(e, data) {
+            // In case the active tile is not within the window we should center it
+            // (note this might trigger even more tiles to load)
+            carousel.goToActiveTile();
 
-        var isEndEvent = e.namespace === 'end';
-        var dataAttr = isEndEvent ? settings.nextAttr : settings.prevAttr;
-        var url = $container.data(dataAttr);
-        if (!url) { return; }
-          
-        $.ajax({
-          'method' : 'get',
-          'url'    : $container.data(dataAttr),
-          'cache'  : false
-        }).done(function(html) {
-            
-            var $searchCarousel = $(html).find(settings.containerSelector).first();
-            var url = $searchCarousel.data(dataAttr);
+            // Add more tiles via ajax when carousel ends
+            $container.on('carousel.end carousel.begin', function(e, data) {
 
-            // Update the data attribute with the URL to fetch the next set of results
-            // Note we use jQuery.data() to set the data,
-            // but we also change the data- attribute on the elment so it can be used by CSS.
-            $container.data(dataAttr, url).attr('data-' + dataAttr, url);
+                var isEndEvent = e.namespace === 'end';
+                var dataAttr = isEndEvent ? settings.nextAttr : settings.prevAttr;
+                var url = $container.data(dataAttr);
+                if (!url) { return; }
+                
+                $.ajax({
+                    'method' : 'get',
+                    'url'    : $container.data(dataAttr),
+                    'cache'  : false
+                }).done(function(html) {
+                    
+                    var $searchCarousel = $(html).find(settings.containerSelector).first();
+                    var url = $searchCarousel.data(dataAttr);
 
-            addTiles($searchCarousel.children(settings.itemsSelector), !isEndEvent);
-            data.carousel.update();
-        });
+                    // Update the data attribute with the URL to fetch the next set of results
+                    // Note we use jQuery.data() to set the data,
+                    // but we also change the data- attribute on the elment so it can be used by CSS.
+                    $container.data(dataAttr, url).attr('data-' + dataAttr, url);
 
-      });
+                    addTiles($searchCarousel.children(settings.itemsSelector), !isEndEvent);
+                    data.carousel.update();
+                });
 
-      /**
-       * Appends or prepends tiles to carousel
-       *
-       * @param {Array|jQueryObject} tiles
-       * jQuery object array
-       *
-       * @param {Boolean} [isPrepend]
-       * Optionally specifies to prepend
-       */
-      function addTiles(tiles, isPrepend) {
+            });
 
-        var $tiles = $(tiles);
+            /**
+             * Appends or prepends tiles to carousel
+             *
+             * @param {Array|jQueryObject} tiles
+             * jQuery object array
+             *
+             * @param {Boolean} [isPrepend]
+             * Optionally specifies to prepend
+             */
+            function addTiles(tiles, isPrepend) {
 
-        if (isPrepend) {
-          $tiles = $tiles.get().reverse();
+                var $tiles = $(tiles);
+
+                if (isPrepend) {
+                    $tiles = $tiles.get().reverse();
+                }
+
+                // Adds tiles to carousel
+                $.each($tiles, function(i, elem) {
+                    var $elem = $(elem);
+                    var index;
+                    
+                    if (isPrepend) {
+                        carousel.prependTile($elem);
+                    } else {
+                        carousel.addTile($elem);
+                    }
+
+                    // Sets active carousel item
+                    if ($elem.find('.carousel-tile-content-active').size() > 0) {
+                        index = ($elem.closest('.carousel-tile').index() + 1) || 0;
+                        carousel.setActive(index);
+                    }
+                });
+            }
         }
-
-        // Adds tiles to carousel
-        $.each($tiles, function(i, elem) {
-          var $elem = $(elem);
-          var index;
-            
-          if (isPrepend) {
-            carousel.prependTile($elem);
-          } else {
-            carousel.addTile($elem);
-          }
-
-          // Sets active carousel item
-          if ($elem.find('.carousel-tile-content-active').size() > 0) {
-              index = ($elem.closest('.carousel-tile').index() + 1) || 0;
-              carousel.setActive(index);
-          }
-        });
-      }
-    }
-  });
+    });
 
 });
