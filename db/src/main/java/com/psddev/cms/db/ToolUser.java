7f45b3f17ddfa481cc27860adf38dfc7de4b7471
Hyoo Lim <hyoolim@gmail.com>
2015-02-19 13:44:39 -0500

    Removes unused import.

--- db/src/main/java/com/psddev/cms/db/ToolUser.java
+++ db/src/main/java/com/psddev/cms/db/ToolUser.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
-import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
