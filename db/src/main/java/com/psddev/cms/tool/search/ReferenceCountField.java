a7ba243b0440f219bb39ab1ed7d6c74b51f8508b
Brendan Brelsford <brendan@perfectsensedigital.com>
2015-05-01 09:50:41 -0400

    Adds ReferenceCountField implementation of SearchResultField to port functionality from full-screen search into 3.0 SearchResultView pattern.

---
+++ db/src/main/java/com/psddev/cms/tool/search/ReferenceCountField.java
@@ -0,0 +1,43 @@
+package com.psddev.cms.tool.search;
+
+import com.psddev.cms.db.Content;
+import com.psddev.cms.tool.SearchResultField;
+import com.psddev.cms.tool.ToolPageContext;
+import com.psddev.dari.db.ObjectType;
+import com.psddev.dari.db.Query;
+import com.psddev.dari.db.State;
+
+import java.io.IOException;
+
+public class ReferenceCountField implements SearchResultField {
+
+    @Override
+    public String getDisplayName() {
+        return "# Of References";
+    }
+
+    @Override
+    public boolean isSupported(ObjectType type) {
+        return Content.Static.isSearchableType(type);
+    }
+
+    @Override
+    public void writeDataCellText(ToolPageContext page, Object item) throws IOException {
+        page.write(String.valueOf(getReferencesCount(item)));
+    }
+
+    @Override
+    public void writeTableDataCellHtml(ToolPageContext page, Object item) throws IOException {
+        page.writeStart("td");
+        page.writeHtml(String.format("%,d", getReferencesCount(State.getInstance(item))));
+        page.writeEnd();
+    }
+
+    private long getReferencesCount(Object item) {
+
+        return Query.
+                fromAll().
+                where("* matches ?", State.getInstance(item).getId()).
+                count();
+    }
+}
