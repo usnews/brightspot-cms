a2c85fa9d867712d22f3b525b83f5106c5683049
Brendan Brelsford <brendan@perfectsensedigital.com>
2015-05-05 14:11:04 -0400

    Adds SearchResultSelection API to clear, add items, and remove items.

--- db/src/main/java/com/psddev/cms/tool/SearchResultSelection.java
+++ db/src/main/java/com/psddev/cms/tool/SearchResultSelection.java
@@ -39,4 +39,62 @@ public class SearchResultSelection extends Record {
 
         return Query.fromAll().where("_id = ?", itemIds);
     }
+
+    /**
+     * Clear the SearchResultSelection by deleting all of the SearchResultSelectionItem that point to it.
+     */
+    public void clear() {
+        Query.
+            from(SearchResultSelectionItem.class).
+            where("selectionId = ?", getId()).
+            deleteAll();
+    }
+
+    /**
+     * Adds the Object with itemId to this SearchResultSelection.
+     * @param itemId the id of the Object to be added.
+     */
+    public boolean addItem(UUID itemId) {
+
+        SearchResultSelectionItem item = Query.
+                from(SearchResultSelectionItem.class).
+                where("selectionId = ?", getId()).
+                and("itemId = ?", itemId).
+                first();
+
+        if (item == null) {
+
+            item = new SearchResultSelectionItem();
+
+            item.setSelectionId(getId());
+            item.setItemId(itemId);
+            item.save();
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Removes the Object with itemId from this SearchResultSelection.
+     * @param itemId the id of the Object to be removed.
+     */
+    public boolean removeItem(UUID itemId) {
+
+        SearchResultSelectionItem item = Query.
+            from(SearchResultSelectionItem.class).
+            where("selectionId = ?", getId()).
+            and("itemId = ?", itemId).first();
+
+        if (item == null) {
+            return false;
+        }
+
+        item.delete();
+        return true;
+    }
+
+    public int size() {
+        return Long.valueOf(createItemsQuery().count()).intValue();
+    }
 }
