22aef98adcb0ae89e657606fc0df97c51837ba45
Brendan Brelsford <brendan@perfectsensedigital.com>
2015-05-07 15:11:25 -0400

    Adds name field, default label and discovery methods to SearchResultSelection.

--- db/src/main/java/com/psddev/cms/tool/SearchResultSelection.java
+++ db/src/main/java/com/psddev/cms/tool/SearchResultSelection.java
@@ -2,17 +2,32 @@ package com.psddev.cms.tool;
 
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
 import com.psddev.cms.db.ToolEntity;
+import com.psddev.cms.db.ToolUser;
 import com.psddev.dari.db.Query;
 import com.psddev.dari.db.Record;
+import com.psddev.dari.util.ObjectUtils;
 
 public class SearchResultSelection extends Record {
 
+    private static final String DEFAULT_LABEL = "New Selection";
+
+    private String name;
+
     @Indexed
     private Set<ToolEntity> entities;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public Set<ToolEntity> getEntities() {
         if (entities == null) {
@@ -22,6 +37,7 @@ public class SearchResultSelection extends Record {
     }
 
     public void setEntities(Set<ToolEntity> entities) {
+
         this.entities = entities;
     }
 
@@ -96,5 +112,42 @@ public class SearchResultSelection extends Record {
 
     public int size() {
         return Long.valueOf(createItemsQuery().count()).intValue();
+    }
+
+    public static List<SearchResultSelection> findOwnSelections(ToolUser user) {
+
+        if (user == null) {
+            return null;
+        }
+
+        return Query.from(SearchResultSelection.class).where("entities = ?", user).selectAll();
+    }
+
+    public static List<SearchResultSelection> findAccessibleSelections(ToolUser user, boolean excludeOwn) {
+
+        if (user == null) {
+            return null;
+        }
+
+        Query<SearchResultSelection> query = Query.from(SearchResultSelection.class);
+
+        if (user.getRole() == null) {
+            query.where("entities != missing");
+        } else {
+            query.where("entities = ?", user.getRole());
+        }
+
+        if (excludeOwn) {
+            query.and("entities != ?", user);
+        } else {
+            query.or("entities = ?", user);
+        }
+
+        return query.selectAll();
+    }
+
+    @Override
+    public String getLabel() {
+        return ObjectUtils.firstNonBlank(getName(), DEFAULT_LABEL);
     }
 }
