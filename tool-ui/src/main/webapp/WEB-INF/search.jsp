usnews/brightspot-cms

commit c85d8bb19f55d19f9d70df8769f70a2a815e3abf

Changes search to use writeObjectSelect to support ToolUi.DropDown.

tool-ui/src/main/webapp/WEB-INF/search.jsp
@@ -446,21 +446,14 @@ writer.start("div", "class", "searchForm");
                                         "checked", filterValue != null && ObjectUtils.to(boolean.class, filterValue.get("m")) ? "checked" : null);
 
                             } else {
-                                State fieldState = State.getInstance(Query.from(Object.class).where("_id = ?", fieldValue).first());
 
-                                writer.writeElement("input",
-                                        "type", "text",
-                                        "class", "objectId",
+                                wp.writeObjectSelect(field, fieldValue,
                                         "name", inputName,
                                         "placeholder", displayName,
-                                        "data-additional-query", field.getPredicate(),
+                                        "data-dynamic-placeholder", "",
                                         "data-editable", false,
-                                        "data-label", fieldState != null ? fieldState.getLabel() : null,
-                                        "data-pathed", ToolUi.isOnlyPathed(field),
-                                        "data-restorable", false,
-                                        "data-searcher-path", fieldUi.getInputSearcherPath(),
-                                        "data-typeIds", fieldTypeIds,
-                                        "value", fieldValue);
+                                        "data-restorable", false);
+
                                 writer.writeElement("input",
                                         "type", "checkbox",
                                         "name", inputName + ".m",

commit bf203592777230190ea33cbe2e5031f4e550e115

Passes fieldState in to writeObjectSelect instead of UUID.
Fixes bug introduced in c85d8bb.

tool-ui/src/main/webapp/WEB-INF/search.jsp
@@ -446,8 +446,9 @@ writer.start("div", "class", "searchForm");
                                         "checked", filterValue != null && ObjectUtils.to(boolean.class, filterValue.get("m")) ? "checked" : null);
 
                             } else {
+                                State fieldState = State.getInstance(Query.from(Object.class).where("_id = ?", fieldValue).first());
 
-                                wp.writeObjectSelect(field, fieldValue,
+                                wp.writeObjectSelect(field, fieldState,
                                         "name", inputName,
                                         "placeholder", displayName,
                                         "data-dynamic-placeholder", "",