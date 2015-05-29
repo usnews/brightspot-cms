bffcffe05bf67ab7b4ae0ea92c10badf62eb38e3
Ryan-Anderson <ryan@perfectsensedigital.com>
2015-05-29 13:35:07 -0400

    Adds js for performing upload and displaying progress indicator

--- tool-ui/src/main/webapp/script/v3/upload.js
+++ tool-ui/src/main/webapp/script/v3/upload.js
@@ -7,7 +7,188 @@ function ($, bsp_utils, evaporate) {
 
   bsp_utils.onDomInsert(document, '.evaporate', {
 
+    insert: function (input) {
 
+      $(input).on('change', function (event) {
+        var $this = $(this);
+        var files = event.target.files;
+        var $inputSmall = $this.closest('.inputSmall');
+        var isMultiple = $this.attr('multiple') ? true : false;
+
+        for (var i = 0; i < files.length; i++) {
+          var file = files[i];
+
+          _beforeUpload(file, $inputSmall, i);
+          var filePath = $this.attr('data-path-start') + "/" + encodeURIComponent(file.name);
+
+          (function ($this, file, filePath, i) {
+            window._e_.add({
+              name: filePath,
+              file: file,
+              notSignedHeadersAtInitiate: {
+                'Cache-Control': 'max-age=3600'
+              },
+              xAmzHeadersAtInitiate: {
+                'x-amz-acl': 'public-read'
+              },
+              progress: function (progress) {
+                _progress($inputSmall, i, Math.round(Number(progress * 100)));
+              },
+              complete: function () {
+                if (isMultiple) {
+                  //_afterBulkUpload($this, $inputSmall, filePath, i);
+                } else {
+                  //_afterUpload($this, $inputSmall, filePath);
+                }
+              }
+
+            });
+          })($this, file, filePath, i);
+        }
+      });
+
+      function _beforeUpload(file, $inputSmall, index) {
+        var $fileSelector = $inputSmall.find('.fileSelector').first();
+
+          $inputSmall.append(_createProgressHtml());
+          var $uploadPreview = $inputSmall.find('.upload-preview').eq(index);
+
+          if (file.type.match('image.*')) {
+            _displayImgPreview($uploadPreview.find('img').first(), file);
+          } else {
+            _displayDefaultPreview($uploadPreview);
+            $uploadPreview.addClass('loading');
+          }
+
+          var $select = $fileSelector.find('select').first();
+
+          if ($select.find('option[value="keep"]').size() < 1) {
+            $select.prepend($('<option/>', {
+              'data-hide': '.fileSelectorItem',
+              'data-show': '.fileSelectorExisting',
+              'value': 'keep',
+              'text': 'Keep Existing'
+            }));
+          }
+
+          $select.val('keep');
+      }
+
+      function _afterUpload($this, $inputSmall, filePath) {
+        var $uploadPreview = $inputSmall.find('.upload-preview');
+        var inputName = $this.attr('data-input-name');
+        var localSrc = $uploadPreview.find('img').first().attr('src');
+
+        var params = {};
+        params['isNewUpload'] = true;
+        params['inputName'] = inputName;
+        params['fieldName'] = $this.attr('data-field-name');
+        params['typeId'] = $this.attr('data-type-id');
+        params[inputName + '.path'] = filePath;
+        params[inputName + '.storage'] = $this.attr('data-storage');
+
+        $uploadPreview.removeClass('loading');
+
+        $.ajax({
+          url: '/cms/storageItemField',
+          dataType: 'html',
+          data: params
+        }).done(function (html) {
+          $uploadPreview.detach();
+          $inputSmall.append(html);
+
+          //prevent image pop-in
+          var img = $inputSmall.find('.imageEditor-image').find('img').first();
+          var remoteSrc = img.attr('src');
+          img.attr('src', localSrc);
+          $.ajax({
+            url: remoteSrc
+          }).done(function (html) {
+            img.attr('src', remoteSrc);
+          });
+        });
+      }
+
+      function _afterBulkUpload($this, $inputSmall, filePath, index) {
+        var $uploadPreview = $inputSmall.find('.upload-preview').eq(index);
+        $uploadPreview.removeClass('loading');
+        var inputName = "file";
+
+        $this.detach();
+
+        var params = {};
+        params['writeInputsOnly'] = true;
+        params['inputName'] = inputName;
+        params[inputName + '.path'] = filePath;
+
+        $.ajax({
+          url: '/cms/content/uploadFiles',
+          dataType: 'html',
+          data: params
+        }).done(function (html) {
+          $inputSmall.prepend(html);
+        });
+      }
+
+      function _progress($inputSmall, i, percentageComplete) {
+        $inputSmall.find('[data-progress]').eq(i).attr('data-progress', percentageComplete);
+      }
+
+      function _displayImgPreview(img, file) {
+
+        if (!(window.File && window.FileReader && window.FileList)) {
+          return;
+        }
+
+        var reader = new FileReader();
+        reader.onload = (function (readFile) {
+          return function (event) {
+            img.attr('src', event.target.result);
+            img.closest('.upload-preview').addClass('loading');
+          };
+        })(file);
+
+        reader.readAsDataURL(file);
+      }
+
+      function _createProgressHtml() {
+        return $('<div/>', {'class' : 'upload-preview'}).append(
+                 $('<div/>', {'class' : 'upload-progress'}).append(
+                   $('<img/>')
+                 ).append(
+                   $('<div/>', {'class' : 'radial-progress', 'data-progress' : '0' }).append(
+                     $('<div/>', {'class' : 'circle'}).append(
+                       $('<div/>', {'class' : 'mask full'}).append(
+                         $('<div/>', {'class' : 'fill'})
+                       )
+                     ).append(
+                       $('<div/>', {'class' : 'mask half'}).append(
+                         $('<div/>', {'class': 'fill'})
+                       ).append(
+                         $('<div/>', {'class': 'fill fix'})
+                       )
+                     )
+                   ).append(
+                     $('<div/>', {'class': 'inset'}).append(
+                       $('<div/>', {'class': 'percentage'}).append(
+                         $('<span/>')
+                       )
+                     )
+                   )
+                 )
+               );
+
+      }
+
+      function _displayDefaultPreview(uploadPreview) {
+        var $uploadPreview = $(uploadPreview);
+        var $uploadPreviewWrapper = $uploadPreview.find('.upload-progress').first();
+
+        $uploadPreview.width(150).height(150);
+        $uploadPreviewWrapper.width(150).height(150);
+
+      }
+    }
   });
 
 });
