d5ee704ece8f2bd79f6c6a5c2384e8515431a7e8
Hyoo Lim <hyoolim@gmail.com>
2015-06-22 11:45:18 -0400

    Adds Maven deployment and Slack notification to Travis CI.

---
+++ etc/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e -u
+
+if [[ "$TRAVIS_REPO_SLUG" == "perfectsense/*" ]] && \
+   [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
+   [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  echo "Deploying to Maven repository..."
+  mvn --settings='etc/settings.xml' javadoc:aggregate source:aggregate deploy:deploy
+  echo "Deployment to Maven repository successful!"
+fi
