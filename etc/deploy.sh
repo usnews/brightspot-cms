490818c806e220d587fe8545d081800967017631
Hyoo Lim <hyoolim@gmail.com>
2015-06-22 12:05:13 -0400

    More tweaks for Travis CI.

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -2,11 +2,11 @@
 
 set -e -u
 
-if [[ "$TRAVIS_REPO_SLUG" == "perfectsense/*" ]] && \
+if [ "$TRAVIS_REPO_SLUG" == "perfectsense/brightspot-cms" ] && \
    [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
    [ "$TRAVIS_BRANCH" == "master" ]; then
 
   echo "Deploying to Maven repository..."
-  mvn --settings='etc/settings.xml' javadoc:aggregate source:aggregate deploy:deploy
+  mvn -o --settings='etc/settings.xml' javadoc:aggregate source:aggregate deploy:deploy
   echo "Deployment to Maven repository successful!"
 fi
