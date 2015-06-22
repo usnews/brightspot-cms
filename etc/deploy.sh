b582b512c55bb904f01b379b9415f08d33ced6dc
Hyoo Lim <hyoolim@gmail.com>
2015-06-22 13:22:03 -0400

    Removes offline mode on Maven deploy script.

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -7,6 +7,6 @@ if [ "$TRAVIS_REPO_SLUG" == "perfectsense/brightspot-cms" ] && \
    [ "$TRAVIS_BRANCH" == "master" ]; then
 
   echo "Deploying to Maven repository..."
-  mvn -o --settings='etc/settings.xml' javadoc:aggregate source:aggregate deploy:deploy
+  mvn --settings='etc/settings.xml' javadoc:aggregate source:aggregate deploy:deploy
   echo "Deployment to Maven repository successful!"
 fi
