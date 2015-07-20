97e00f25b0f83257c809d7be66519cf8b97396bf
Patrick Fitzgerald <pat@perfectsensedigital.com>
2015-07-20 17:10:26 -0400

    RTE2 newline handling for raw html
    Modified HTML output and input to better support raw html.
    For input: leaving newline characters in the input to force a new line
    For output:
    If a line ends with a character marked as raw HTML, do not add a <br> at the end of the line, instead add a newline character \n
    For other lines, add a <br> to the end of the line
    For lines that end with a block level element (like a UL or OL list) do not output a newline character.
    
    This seems to prevent extra newlines from being added to the editor when importing arbitrary HTML for the most common cases. I don't think it will be possible to handle all cases perfectly, but this seems to be the best option.

--- tool-ui/src/main/webapp/script/v3/input/richtextCodeMirror.js
+++ tool-ui/src/main/webapp/script/v3/input/richtextCodeMirror.js
@@ -2650,7 +2650,7 @@ define(['jquery', 'codemirror/lib/codemirror'], function($, CodeMirror) {
             // Loop through the content one line at a time
             doc.eachLine(function(line) {
 
-                var annotationStart, annotationEnd, blockOnThisLine, htmlStartOfLine, htmlEndOfLine, inlineActive, inlineElementsToClose, lineNo, outputChar, raw;
+                var annotationStart, annotationEnd, blockOnThisLine, htmlStartOfLine, htmlEndOfLine, inlineActive, inlineElementsToClose, lineNo, outputChar, raw, rawLastChar;
 
                 lineNo = line.lineNo();
                 
@@ -2706,7 +2706,7 @@ define(['jquery', 'codemirror/lib/codemirror'], function($, CodeMirror) {
                                 // We are not already inside this style, so create the container element
                                 // and remember that we created it
                                 blockActive[container] = true;
-                                htmlStartOfLine += '<' + container + '>\n';
+                                htmlStartOfLine += '<' + container + '>';
                             }
                         }
 
@@ -2730,7 +2730,7 @@ define(['jquery', 'codemirror/lib/codemirror'], function($, CodeMirror) {
                     if (!blockOnThisLine[container]) {
                         delete blockActive[container];
                         if (container) {
-                            html += '</' + container + '>\n';
+                            html += '</' + container + '>';
                         }
                     }
                 });
@@ -2901,16 +2901,27 @@ define(['jquery', 'codemirror/lib/codemirror'], function($, CodeMirror) {
                     }
 
                     outputChar = line.text.charAt(charNum);
-                    if (raw) {
 
-                        if (outputChar === '<') {
-                            outputChar = '&raw_lt;';
+                    // In some cases (at end of line) output char might be empty
+                    if (outputChar) {
+                        
+                        if (raw) {
+
+                            if (outputChar === '<') {
+                                outputChar = '&raw_lt;';
+                            }
+
+                            rawLastChar = true;
+                        
+                        } else {
+                        
+                            outputChar = self.htmlEncode(outputChar);
+                        
+                            rawLastChar = false;
                         }
-
-                    } else {
-                        outputChar = self.htmlEncode(outputChar);
+                        
+                        html += outputChar;
                     }
-                    html += outputChar;
                 }
 
                 // If we reached end of line, close all the open block elements
@@ -2920,11 +2931,13 @@ define(['jquery', 'codemirror/lib/codemirror'], function($, CodeMirror) {
                         var element;
                         element = this.element;
                         if (element) {
-                            html += '</' + element + '>\n';
+                            html += '</' + element + '>';
                         }
                     });
                     blockElementsToClose = [];
                     
+                } else if (rawLastChar) {
+                    html += '\n';
                 } else {
                     // No block elements so add a line break
                     html += '<br/>';
@@ -2941,7 +2954,7 @@ define(['jquery', 'codemirror/lib/codemirror'], function($, CodeMirror) {
             $.each(blockActive, function(container) {
                 delete blockActive[container];
                 if (container) {
-                    html += '</' + container + '>\n';
+                    html += '</' + container + '>';
                 }
             });
 
@@ -2997,7 +3010,8 @@ define(['jquery', 'codemirror/lib/codemirror'], function($, CodeMirror) {
                         // We got a text node, just add it to the value
                         // Remove any newlines at the beginning or end
                         // Remove "zero width space" character that the previous editor sometimes used
-                        val += next.textContent.replace(/^[\n\r]+|[\n\r]+$/g, '').replace(/\u200b|\u8203/g, '');
+                        //val += next.textContent.replace(/^[\n\r]+|[\n\r]+$/g, '').replace(/\u200b|\u8203/g, '');
+                        val += next.textContent.replace(/\u200b|\u8203/g, '');
                         
                     } else if (next.nodeType === 1) {
 
