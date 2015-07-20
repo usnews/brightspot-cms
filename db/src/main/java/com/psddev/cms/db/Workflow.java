4f068f5c5507b54550209cf2bd77e09b49ca5533
Hyoo Lim <hyoolim@gmail.com>
2015-07-20 09:15:44 -0400

    Removes unused import.

--- db/src/main/java/com/psddev/cms/db/Workflow.java
+++ db/src/main/java/com/psddev/cms/db/Workflow.java
@@ -8,7 +8,6 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.UUID;
 
 import com.psddev.dari.db.Modification;
 import com.psddev.dari.db.ObjectField;
