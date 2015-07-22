4d9879c08891add253dcac32720bb01dfd4b3bc3
Hyoo Lim <hyoolim@gmail.com>
2015-07-22 10:19:56 -0400

    Adds PublishBroadcast for field locking on publish.

--- tool-ui/src/main/webapp/script/v3/content/edit.js
+++ tool-ui/src/main/webapp/script/v3/content/edit.js
@@ -1,13 +1,13 @@
 define([ 'jquery', 'v3/rtc' ], function($, rtc) {
 
-  rtc.receive('com.psddev.cms.tool.page.content.EditFieldUpdateBroadcast', function(data) {
-    var userId = data.userId;
-    var userName = data.userName;
-    var fieldNamesByObjectId = data.fieldNamesByObjectId;
-
+  function removeStatuses(userId) {
     $('.inputStatus[data-user-id="' + userId + '"]').each(function() {
       var $status = $(this);
       var $container = $status.closest('.inputContainer');
+
+      if ($container.is('.inputContainer-lock')) {
+        return;
+      }
 
       if (!$container.is('.inputContainer-readOnly')) {
         $container.find(':input').prop('disabled', false);
@@ -15,6 +15,30 @@ define([ 'jquery', 'v3/rtc' ], function($, rtc) {
 
       $status.remove();
     });
+  }
+
+  function updateStatus($container, userId, message) {
+    if ($container.length === 0 || $container.is('.inputContainer-lock')) {
+      return;
+    }
+
+    if (!$container.is('.inputContainer-readOnly')) {
+      $container.find(':input').prop('disabled', true);
+    }
+
+    $container.find('> .inputLabel').after($('<div/>', {
+      'class': 'inputStatus',
+      'data-user-id': userId,
+      'text': message
+    }));
+  }
+
+  rtc.receive('com.psddev.cms.tool.page.content.EditFieldUpdateBroadcast', function(data) {
+    var userId = data.userId;
+    var userName = data.userName;
+    var fieldNamesByObjectId = data.fieldNamesByObjectId;
+
+    removeStatuses(userId);
 
     if (!fieldNamesByObjectId) {
       return;
@@ -24,19 +48,34 @@ define([ 'jquery', 'v3/rtc' ], function($, rtc) {
       var $inputs = $('.objectInputs[data-id="' + objectId + '"]');
 
       $.each(fieldNames, function (i, fieldName) {
-        var $container = $inputs.find('> .inputContainer[data-field="' + fieldName + '"]');
-
-        if (!$container.is('.inputContainer-readOnly')) {
-          $container.find(':input').prop('disabled', true);
-        }
-
-        $container.find('> .inputLabel').after($('<div/>', {
-          'class': 'inputStatus',
-          'data-user-id': userId,
-          'text': 'Pending edit from ' + userName
-        }));
+        updateStatus(
+            $inputs.find('> .inputContainer[data-field="' + fieldName + '"]'),
+            userId,
+            'Pending edit from ' + userName);
       });
     });
+  });
+
+  rtc.receive('com.psddev.cms.tool.page.content.PublishBroadcast', function(data) {
+    var newValues = data.values;
+    var contentId = data.contentId;
+    var oldValues = $('input[name="' + contentId + '/oldValues"]').val();
+
+    if (oldValues) {
+      var userId = data.userId;
+      var userName = data.userName;
+
+      removeStatuses(userId);
+
+      $.each($.parseJSON(oldValues), function(fieldName, oldValue) {
+        if (JSON.stringify(oldValue) !== JSON.stringify(newValues[fieldName])) {
+          var $container = $('.objectInputs[data-id="' + contentId + '"] > .inputContainer[data-field="' + fieldName + '"]');
+
+          updateStatus($container, userId, 'Updated by ' + userName + ' at ' + new Date(data.date));
+          $container.addClass('inputContainer-lock');
+        }
+      });
+    }
   });
 
   $('.contentForm').each(function() {
