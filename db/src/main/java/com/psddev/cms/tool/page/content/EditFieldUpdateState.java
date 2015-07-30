9f62ba90b8a0cc1deb93ecbe27b805de29af6d44
Hyoo Lim <hyoolim@gmail.com>
2015-07-30 13:40:37 -0400

    Adds time tracking to EditFieldUpdate so that users can't hold onto the lock for too long.

--- db/src/main/java/com/psddev/cms/tool/page/content/EditFieldUpdateState.java
+++ db/src/main/java/com/psddev/cms/tool/page/content/EditFieldUpdateState.java
@@ -1,16 +1,44 @@
 package com.psddev.cms.tool.page.content;
 
 import com.psddev.cms.tool.page.RtcState;
+import com.psddev.dari.db.Database;
 import com.psddev.dari.db.Query;
+import org.joda.time.DateTime;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 class EditFieldUpdateState implements RtcState {
 
     @Override
     public Iterable<? extends Object> create(Map<String, Object> data) {
-        return Query.from(EditFieldUpdate.class)
+        List<EditFieldUpdate> updates = Query.from(EditFieldUpdate.class)
                 .where("contentId = ?", data.get("contentId"))
                 .selectAll();
+
+        Database database = Database.Static.getDefault();
+        DateTime expire = new DateTime(database.now()).minusSeconds(60);
+
+        database.beginIsolatedWrites();
+
+        try {
+            for (Iterator<EditFieldUpdate> it = updates.iterator(); it.hasNext(); ) {
+                EditFieldUpdate update = it.next();
+                long updateTime = update.getTime();
+
+                if (updateTime == 0 || new DateTime(updateTime).isBefore(expire)) {
+                    it.remove();
+                    update.delete();
+                }
+            }
+
+            database.commitWrites();
+
+        } finally {
+            database.endWrites();
+        }
+
+        return updates;
     }
 }
