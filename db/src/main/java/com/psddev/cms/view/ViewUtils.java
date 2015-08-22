84e50a915338dfddb421c0a845c4d6765dcca0c5
Dan Slaughter <dan@perfectsensedigital.com>
2015-08-22 14:18:38 -0400

    Broadens the scope of ViewUtils#getAnnotatableClasses search.

--- db/src/main/java/com/psddev/cms/view/ViewUtils.java
+++ db/src/main/java/com/psddev/cms/view/ViewUtils.java
@@ -5,35 +5,37 @@ import com.psddev.dari.db.Recordable;
 import com.psddev.dari.util.ObjectUtils;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 final class ViewUtils {
 
     private ViewUtils() {
     }
 
-    // Returns classes in order of input class, followed by its super classes,
-    // followed by its interfaces, followed by its modification classes.
+    // Returns classes in order of input class followed by all interfaces and
+    // super interfaces, followed by its super classes and their interfaces
+    // and super interfaces, followed by its modification classes.
     static List<Class<?>> getAnnotatableClasses(Class<?> klass) {
 
-        List<Class<?>> classesToCheck = new ArrayList<>();
+        Set<Class<?>> classesToCheck = new LinkedHashSet<>();
 
+        // add class and interfaces
         classesToCheck.add(klass);
+        classesToCheck.addAll(getAllInterfaces(klass));
 
-        // check super classes
+        // repeat for each super class
         Class<?> superClass = klass.getSuperclass();
-
         while (superClass != null) {
             if (!Object.class.equals(superClass)) {
                 classesToCheck.add(superClass);
+                classesToCheck.addAll(getAllInterfaces(superClass));
             }
 
             superClass = superClass.getSuperclass();
         }
-
-        // check interfaces
-        classesToCheck.addAll(Arrays.asList(klass.getInterfaces()));
 
         // check modification classes
         if (Recordable.class.isAssignableFrom(klass)) {
@@ -48,6 +50,20 @@ final class ViewUtils {
             }
         }
 
-        return classesToCheck;
+        return new ArrayList<>(classesToCheck);
+    }
+
+    // fetches the interfaces for the given class including including any super
+    // interfaces.
+    private static Set<Class<?>> getAllInterfaces(Class<?> klass) {
+
+        Set<Class<?>> interfaces = new HashSet<>();
+
+        for (Class<?> interfaceClass : klass.getInterfaces()) {
+            interfaces.add(interfaceClass);
+            interfaces.addAll(getAllInterfaces(interfaceClass));
+        }
+
+        return interfaces;
     }
 }
