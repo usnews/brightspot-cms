0b4d06efb01336d88f9e476c9f47775c8020dc19
Ryan-Anderson <ryan@perfectsensedigital.com>
2015-09-04 12:00:27 -0400

    Localizes ContentExternalPreview

--- db/src/main/java/com/psddev/cms/tool/page/ContentExternalPreview.java
+++ db/src/main/java/com/psddev/cms/tool/page/ContentExternalPreview.java
@@ -26,7 +26,7 @@ public class ContentExternalPreview extends PageServlet {
             page.writeStart("div", "class", "widget");
                 page.writeStart("h1",
                         "class", "icon icon-action-preview");
-                    page.writeHtml("External Content Preview");
+                    page.writeHtml(page.localize(null, "contentExternalPreview.title"));
                 page.writeEnd();
 
                 page.writeStart("iframe",
