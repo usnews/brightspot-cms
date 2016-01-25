ddbd88f19e9adeebf7e66cd94718b838c0b240b0
Hyoo Lim <hyoolim@gmail.com>
2016-01-25 14:52:51 -0500

    Adds BE hooks for void elements in RTE.

--- db/src/main/java/com/psddev/cms/db/RichTextElement.java
+++ db/src/main/java/com/psddev/cms/db/RichTextElement.java
@@ -21,6 +21,7 @@ public abstract class RichTextElement extends Record {
     public @interface Tag {
 
         String value();
+        boolean empty() default false;
         boolean root() default true;
         String[] parents() default { };
         String menu() default "";
--- db/src/main/java/com/psddev/cms/tool/ToolPageContext.java
+++ db/src/main/java/com/psddev/cms/tool/ToolPageContext.java
@@ -1951,6 +1951,7 @@ public class ToolPageContext extends WebPageContext {
                 ObjectType type = ObjectType.getInstance(c);
 
                 richTextElement.put("tag", tag.value());
+                richTextElement.put("void", tag.empty());
                 richTextElement.put("popup", type.getFields().stream()
                         .filter(f -> !f.as(ToolUi.class).isHidden())
                         .findFirst()
