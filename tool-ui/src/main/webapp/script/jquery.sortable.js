f0d166402c5ffbc6b23d4bbde541b11859a7dc36
Andrew Miller <andrew@perfectsensedigital.com>
2016-02-24 10:03:51 -0500

    Fixes reordering of Strings.

--- tool-ui/src/main/webapp/script/jquery.sortable.js
+++ tool-ui/src/main/webapp/script/jquery.sortable.js
@@ -6,7 +6,7 @@ var $doc = $(win.document);
 function findRelatedContainers($selected) {
     var itemType = $selected.attr('data-sortable-item-type');
 
-    return itemType ? $('[data-sortable-valid-item-types~="' + itemType + '"]') : $();
+    return itemType ? $('[data-sortable-valid-item-types~="' + itemType + '"]') : $('.plugin-sortable._sortable:not([data-sortable-valid-item-types])');
 }
 
 $.plugin2('sortable', {
