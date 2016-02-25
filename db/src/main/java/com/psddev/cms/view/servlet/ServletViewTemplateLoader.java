404af9b4163ec0c1426c3f8e73d349f33d919d1a
Dan Slaughter <dan@perfectsensedigital.com>
2016-02-25 16:40:37 -0500

    Only store off the ServletContext in case the template loader is cached across requests.

--- db/src/main/java/com/psddev/cms/view/servlet/ServletViewTemplateLoader.java
+++ db/src/main/java/com/psddev/cms/view/servlet/ServletViewTemplateLoader.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
 import com.psddev.cms.view.AbstractViewTemplateLoader;
@@ -14,22 +15,22 @@ import com.psddev.dari.util.CodeUtils;
  */
 public class ServletViewTemplateLoader extends AbstractViewTemplateLoader {
 
-    private HttpServletRequest request;
+    private ServletContext servletContext;
 
     /**
      * @param request the current HTTP request.
      */
     public ServletViewTemplateLoader(HttpServletRequest request) {
-        this.request = request;
+        servletContext = request.getServletContext();
     }
 
     @Override
     public InputStream getTemplateAsStream(String path) {
-        return CodeUtils.getResourceAsStream(request.getServletContext(), path);
+        return CodeUtils.getResourceAsStream(servletContext, path);
     }
 
     @Override
     protected URL getTemplateAsURL(String path) throws MalformedURLException {
-        return CodeUtils.getResource(request.getServletContext(), path);
+        return CodeUtils.getResource(servletContext, path);
     }
 }
